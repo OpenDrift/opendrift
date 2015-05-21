Corrected rotation of vectors onto latlong SRS

models/openDriftSimulation.py
@@ -631,26 +631,30 @@ class OpenDriftSimulation(object):
 
         Arguments:
             x_vel and v_vel: floats, velocities in m/s of particle along
-            x- and y-axes of the inherit SRS (proj4).
+                             x- and y-axes of the inherit SRS (proj4).
         """
 
-        # Calculate x,y from lon,lat
-        self.elements.x, self.elements.y = self.lonlat2xy(
-            self.elements.lon, self.elements.lat)
-        # Update x,y
-        if self.proj.is_latlong():
-            geod = pyproj.Geod(ellps='WGS84')
-            velocity = np.sqrt(x_vel**2 + y_vel**2)
-            azimuth = np.degrees(np.arctan2(x_vel, y_vel))  # Dir. of motion
-            self.elements.x, self.elements.y, back_az = geod.fwd(
-                self.elements.x, self.elements.y,
-                azimuth, velocity*self.time_step.total_seconds())
-        else:
-            self.elements.x += x_vel*self.time_step.total_seconds()
-            self.elements.y += y_vel*self.time_step.total_seconds()
-        # Calculate lon,lat from x,y
-        self.elements.lon, self.elements.lat = self.xy2lonlat(
-            self.elements.x, self.elements.y)
+        geod = pyproj.Geod(ellps='WGS84')
+
+        azimuth = np.degrees(np.arctan2(x_vel, y_vel))  # Direction of motion
+        velocity = np.sqrt(x_vel**2 + y_vel**2)  # Velocity in m/s
+
+        if not self.proj.is_latlong():  # Need to rotate SRS
+            # Calculate x,y from lon,lat
+            self.elements.x, self.elements.y = self.lonlat2xy(
+                self.elements.lon, self.elements.lat)
+            # Calculate azimuth orientation of y-axis at particle locations
+            delta_y = 1000  # Using delta of 1000 m to calculate azimuth
+            lon2, lat2 = self.xy2lonlat(self.elements.x, self.elements.y + delta_y)
+            azimuth_srs = geod.inv(self.elements.lon, self.elements.lat,
+                                   lon2, lat2)[0]
+            azimuth = azimuth + azimuth_srs
+
+        # Calculate new positions
+        self.elements.lon, self.elements.lat, back_az = geod.fwd(
+            self.elements.lon, self.elements.lat,
+            azimuth, velocity*self.time_step.total_seconds())
+
         # Check that new positions are valid
         if (self.elements.lon.min() < -180) or (
                 self.elements.lon.min() > 360) or (

readers/readers.py
@@ -308,23 +308,25 @@ class Reader(object):
             vector_pairs = [list(x) for x in set(tuple(x)
                             for x in vector_pairs)]
             # Calculate azimuth angle between coordinate systems (y-axis)
-            if self.delta_y is None:
-                delta_y = 1000
-            else:
-                delta_y = self.delta_y*.1  # 10% of grid separation
+            delta_y = 1000
             x2, y2 = pyproj.transform(self.proj, rotate_to_proj,
                                       reader_x, reader_y)
             x2_delta, y2_delta = pyproj.transform(self.proj,
                                                   rotate_to_proj,
                                                   reader_x, reader_y + delta_y)
-            rot_angle_rad = np.arctan2(x2_delta - x2, y2_delta - y2)
-            logging.debug('Rotating vectors to srs: "%s"' %
-                          rotate_to_proj.srs)
+            if rotate_to_proj.is_latlong():
+                geod = pyproj.Geod(ellps='WGS84')
+                rot_angle_rad = -np.radians(geod.inv(
+                                    x2, y2, x2_delta, y2_delta)[0])
+            else:
+                rot_angle_rad = -np.arctan2(x2_delta - x2, y2_delta - y2)
+
             for vector_pair in vector_pairs:
-                logging.debug('Rotating %s an angle of %f to %f degrees' %
-                              (str(vector_pair),
+                logging.debug('Rotating %s an angle of %f to %f degrees ' 
+                              ' to srs %s' % (str(vector_pair),
                                np.degrees(rot_angle_rad).min(),
-                               np.degrees(rot_angle_rad).min()))
+                               np.degrees(rot_angle_rad).min(),
+                               rotate_to_proj.srs))
                 u2 = (env[vector_pair[0]]*np.cos(rot_angle_rad) -
                       env[vector_pair[1]]*np.sin(rot_angle_rad))
                 v2 = (env[vector_pair[0]]*np.sin(rot_angle_rad) +