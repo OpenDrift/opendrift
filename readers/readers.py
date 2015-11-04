6639ca34fb63131d167e4cb7ffb7cc4f76ee29fd
Knut-Frode Dagestad <knutfrode@gmail.com>
2015-11-04 18:09:00 +0100

    Added GPL license header to main OpenDrift files.

--- readers/readers.py
+++ readers/readers.py
@@ -1,3 +1,19 @@
+# This file is part of OpenDrift.
+# 
+# OpenDrift is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2
+# 
+# OpenDrift is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with OpenDrift.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Copyright 2015, Knut-Frode Dagestad, MET Norway
+
 import sys
 import importlib
 import logging
--- scripts/readerinfo.py
+++ scripts/readerinfo.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+#
+# This file is part of OpenDrift.
+# 
+# OpenDrift is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2
+# 
+# OpenDrift is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with OpenDrift.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Copyright 2015, Knut-Frode Dagestad, MET Norway
+
 
 # Utility script to display contents of a netCDF CF-compliant
 # file or URL containing driver data suitable for opendrift
