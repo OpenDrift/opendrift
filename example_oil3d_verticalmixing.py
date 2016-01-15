2707cd463a10afce270461c0acfddcfccaade051
Knut-Frode Dagestad <knutfd@met.no>
2016-01-15 13:59:46 +0100

    Made 3D version of OpenOil, incorporating turbulent mixing scheme implemented by Johannes Rohrs.

---
+++ example_oil3d_verticalmixing.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+from datetime import datetime, timedelta
+
+from readers import reader_basemap_landmask
+from readers import reader_netCDF_CF_generic
+from models.pelagicegg import PelagicEggDrift
+from models.openoil3D import OpenOil3D
+
+
+#o = PelagicEggDrift(loglevel=0)  # Set loglevel to 0 for debug information
+o = OpenOil3D(loglevel=0)  # Set loglevel to 0 for debug information
+
+ncfile = 'oil3Dmixing.nc'
+import_file = False
+
+if import_file is True:
+    o.io_import_file(ncfile)
+else:
+    # Arome
+    reader_arome = reader_netCDF_CF_generic.Reader('/disk2/data/opendrift_test_data/15jan2016/AROME_MetCoOp_00_DEF.nc')
+    #reader_arome = reader_netCDF_CF_generic.Reader('/opdata/arome2_5_main/AROME_MetCoOp_00_DEF.nc')
+
+    # Norkyst
+    reader_norkyst = reader_netCDF_CF_generic.Reader('/disk2/data/opendrift_test_data/15jan2016/NorKyst-800m_ZDEPTHS_his_00.nc')
+    #reader_norkyst = reader_netCDF_CF_generic.Reader('/opdata/roms/NorKyst-800m_ZDEPTHS_his_00.nc')
+
+    # Landmask (Basemap)
+    # Stad
+    reader_basemap = reader_basemap_landmask.Reader(
+                        llcrnrlon=3.0, llcrnrlat=61.4,
+                        urcrnrlon=6.2, urcrnrlat=62.6,
+                        resolution='h', projection='merc')
+
+    o.add_reader([reader_norkyst, reader_basemap, reader_arome])
+
+    # Experiment with constant wind instead of arome
+    #o.add_reader([reader_norkyst, reader_basemap])
+    #o.fallback_values['x_wind'] = 20
+    #o.fallback_values['y_wind'] = 20
+
+    # Seeding some particles
+    #lon = 5.0; lat = 60.0; # Outside Bergen
+    #lon = 5.15; lat = 58.85; # Outside Stavanger
+    lon = 4.9; lat = 62.1; # Stad
+    #lon = 13.8; lat = 67.57; # Vestfjorden
+
+    #time = [reader_arome.start_time,
+    #        reader_arome.start_time + timedelta(hours=30)]
+    time = reader_arome.start_time
+
+    # Seed oil elements at defined position and time
+    o.seed_elements(lon, lat, z=-0.5, radius=0, number=3000, time=time,
+                    density=880)
+
+    # Adjusting some configuration
+    o.config['drift']['wind_drift_factor'] = .02
+
+    o.config['processes']['turbulentmixing'] = True
+    o.config['turbulentmixing']['diffusivitymodel'] = 'windspeed_Sundby1983'
+    #o.config['turbulentmixing']['diffusivitymodel'] = 'stepfunction'
+    o.config['turbulentmixing']['timestep'] = 2. # seconds
+
+    o.config['processes']['diffusion'] = False
+    o.config['drift']['current_uncertainty'] = .1
+    o.config['drift']['wind_uncertainty'] = 2
+    o.config['processes']['evaporation'] = True
+    o.config['processes']['dispersion'] = False
+    o.config['processes']['emulsification'] = True
+
+    # Running model (until end of driver data)
+    o.run(steps=66*1, time_step=3600, outfile=ncfile)
+
+###########################
+# Print and plot results
+###########################
+print o
+
+o.plot()
+o.plot(linecolor='z')
+o.animation()
+#o.plot_vertical_distribution()
+o.plot_vertical_distribution_slider()
+
--- models/openoil3D.py
+++ models/openoil3D.py
@@ -18,15 +18,18 @@ import os
 import numpy as np
 from datetime import datetime
 
-from opendrift import OpenDriftSimulation
-from elements import LagrangianArray
+#from opendrift import OpenDriftSimulation
+from opendrift3D import OpenDrift3DSimulation, Lagrangian3DArray
 
 
 # Defining the oil element properties
-class Oil(LagrangianArray):
+class Oil3D(Lagrangian3DArray):
     """Extending LagrangianArray with variables relevant for oil particles."""
 
-    variables = LagrangianArray.add_variables([
+    variables = Lagrangian3DArray.add_variables([
+        ('diameter', {'dtype': np.float32,
+                       'units': 'm',
+                       'default': 0.0001}), # Typical from litterture
         ('mass_oil', {'dtype': np.float32,
                       'units': 'kg',
                       'default': 1}),
@@ -63,7 +66,7 @@ class Oil(LagrangianArray):
                            'default': 0})])
 
 
-class OpenOil(OpenDriftSimulation):
+class OpenOil3D(OpenDrift3DSimulation):
     """Open source oil trajectory model based on the OpenDrift framework.
 
         Developed at MET Norway based on oil weathering parameterisations
@@ -72,20 +75,32 @@ class OpenOil(OpenDriftSimulation):
         Under construction.
     """
 
-    ElementType = Oil
+    ElementType = Oil3D
 
     required_variables = ['x_sea_water_velocity', 'y_sea_water_velocity',
                           'sea_surface_wave_significant_height',
                           'sea_surface_wave_to_direction',
                           'sea_ice_area_fraction',
-                          'x_wind', 'y_wind', 'land_binary_mask']
+                          'x_wind', 'y_wind', 'land_binary_mask',
+                          'sea_floor_depth_below_sea_level',
+                          'ocean_vertical_diffusivity',
+                          'sea_water_temperature',
+                          'sea_water_salinity'
+                          #'upward_sea_water_velocity'
+                          ]
 
     fallback_values = {'x_sea_water_velocity': 0,
                        'y_sea_water_velocity': 0,
                        'sea_surface_wave_significant_height': 0,
                        'sea_surface_wave_to_direction': 0,
                        'sea_ice_area_fraction': 0,
-                       'x_wind': 0, 'y_wind': 0}
+                       'x_wind': 0, 'y_wind': 0,
+                       'sea_floor_depth_below_sea_level': 100,
+                       'ocean_vertical_diffusivity': 0.02, #m2s-1
+                       'sea_water_temperature': 10.,
+                       'sea_water_salinity' : 34.
+                       #'upward_sea_water_velocity': 0
+                       }
 
     # Default colors for plotting
     status_colors = {'initial': 'green', 'active': 'blue',
@@ -110,6 +125,8 @@ class OpenOil(OpenDriftSimulation):
                 time = string(default='%s')
                 oil_type = option(%s, default=%s)
         [processes]
+            turbulentmixing = boolean(default=True)
+            verticaladvection = boolean(default=False)
             dispersion = boolean(default=True)
             diffusion = boolean(default=True)
             evaporation = boolean(default=True)
@@ -119,6 +136,11 @@ class OpenOil(OpenDriftSimulation):
             current_uncertainty = float(min=0, max=5, default=.1)
             wind_uncertainty = float(min=0, max=5, default=1)
             relative_wind = boolean(default=True)
+        [turbulentmixing]
+            timestep = float(min=0.1, max=3600, default=1.)
+            verticalresolution = float(min=0.01, max=10, default = 1.)
+            diffusivitymodel = string(default='environment')
+
     ''' % (datetime.now().strftime('%Y-%d-%m %H:00'), oil_types, default_oil)
 
     def __init__(self, *args, **kwargs):
@@ -139,7 +161,48 @@ class OpenOil(OpenDriftSimulation):
         self.oiltypes_linenumbers = linenumbers
 
         # Calling general constructor of parent class
-        super(OpenOil, self).__init__(*args, **kwargs)
+        super(OpenOil3D, self).__init__(*args, **kwargs)
+
+    def update_terminal_velocity(self):
+        """Calculate terminal velocity for oil droplets
+
+        according to 
+        Tkalich et al. (2002): Vertical mixing of oil droplets by breaking waves
+        Marine Pollution Bulletin 44, 1219-1229
+        """
+        g = 9.81 # ms-2
+
+        r = self.elements.diameter # particle radius
+        T0 = self.environment.sea_water_temperature
+        S0 = self.environment.sea_water_salinity
+        rho_oil = self.elements.density
+        rho_water = sea_water_density(T=T0, S=S0)
+
+        # dynamic water viscosity
+        my_w = 0.001*(1.7915 - 0.0538*T0 + 0.007*(T0**(2.0)) - 0.0023*S0) # ~0.0014 kg m-1 s-1
+        # kinemativ water viscosity
+        ny_w = my_w / rho_water
+        # 
+        rhopr = rho_oil/rho_water
+
+        # terminal velocity for low Reynolds numbers
+        kw = 2*g*(1-rhopr)/(9*ny_w)
+        W = kw * r**2
+        #print r[0:10], 'r'
+        #print ny_w[0:10], 'ny_w'
+        #print W[0:10], 'W before'
+
+        #check if we are in a high Reynolds number regime
+        Re = 2*r*W/ny_w
+        highRe = np.where(Re > 50) 
+        
+        # Terminal velocity in high Reynolds numbers
+        kw = (16*g*(1-rhopr)/3)**0.5
+        W2 = kw*r**0.5
+
+        W[highRe] = W2[highRe]
+        #print W[0:10], 'W after'
+        self.elements.terminal_velocity = W 
 
     def update(self):
         """Update positions and properties of oil particles."""
@@ -273,11 +336,33 @@ class OpenOil(OpenDriftSimulation):
             #                        1/1.17)
             #self.deactivate_elements(droplet_size < 5E-7, reason='dispersed')
 
+        ###################
+        # Turbulent Mixing
+        ###################
+        if self.config['processes']['turbulentmixing'] is True:
+
+            self.update_terminal_velocity()
+            self.vertical_mixing()
+
+        ###################
+        # Vertical advection
+        ###################
+        if self.config['processes']['verticaladvection'] is True:
+
+            self.vertical_advection(self.environment.upward_sea_water_velocity)
+
         ####################################
-        # Deactivate elements hitting land
+        # Deactivate elements hitting coast
         ####################################
         self.deactivate_elements(self.environment.land_binary_mask == 1,
                                  reason='stranded')
+
+        ##########################################
+        # Do not let particles go below seafloor
+        ##########################################
+        self.elements.z[np.where(self.elements.z < \
+            -self.environment.sea_floor_depth_below_sea_level)] = \
+            -self.environment.sea_floor_depth_below_sea_level
 
         ########################################
         ## Deactivate elements hitting sea ice
@@ -436,7 +521,7 @@ class OpenOil(OpenDriftSimulation):
             oiltype = 'BALDER'  # Default
         self.set_oiltype(oiltype)
 
-        super(OpenOil, self).seed_elements(*args, **kwargs)
+        super(OpenOil3D, self).seed_elements(*args, **kwargs)
 
     def seed_from_gml(self, gmlfile, num_elements=1000):
         """Read oil slick contours from GML file, and seed particles within."""
@@ -533,3 +618,36 @@ class OpenOil(OpenDriftSimulation):
         kwargs['ID'] = np.arange(len(lonpoints)) + 1
         kwargs['mass_oil'] = 1
         self.schedule_elements(self.ElementType(**kwargs), oil_time)
+
+def sea_water_density(T=10.,S=35.):
+    '''The function gives the density of seawater at one atmosphere
+    pressure as given in :
+
+    N.P. Fofonoff and R.C. Millard Jr.,1983,
+    Unesco technical papers in marine science no. 44.
+    
+    S   = Salinity in promille of the seawater
+    T   = Temperature of the seawater in degrees Celsius
+    '''
+
+    R4=4.8314E-04
+    DR350=28.106331
+
+    #Pure water density at atmospheric pressure
+    #Bigg P.H. (1967) BR. J. Applied Physics pp.:521-537
+
+    R1 = ((((6.536332E-09*T-1.120083E-06)*T+1.001685E-04)*T-9.095290E-03)*T+6.793952E-02)*T-28.263737
+
+    #Seawater density at atmospheric pressure
+    #coefficients involving salinity :
+
+    R2 = (((5.3875E-09*T-8.2467E-07)*T+7.6438E-05)*T-4.0899E-03)*T +8.24493E-01
+
+    R3 = (-1.6546E-06*T+1.0227E-04)*T-5.72466E-03
+
+    #International one-atmosphere equation of state of seawater :
+
+    SIG = R1 + (R4*S + R3*np.sqrt(S) + R2)*S
+    Dens0 = SIG + DR350 + 1000.
+    return Dens0
+
