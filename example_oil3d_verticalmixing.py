02abdf1ea42f037418be7feed3989cfbaf985db5
Knut-Frode Dagestad <knutfrode@gmail.com>
2016-02-03 13:27:49 +0100

    Updated example_oil3d_verticalmixing to read from Thredds if local file not available

--- example_oil3d_verticalmixing.py
+++ example_oil3d_verticalmixing.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python
 
+import os.path
 from datetime import datetime, timedelta
 
 from readers import reader_basemap_landmask
 from readers import reader_netCDF_CF_generic
 from models.openoil3D import OpenOil3D
 
-################################################################
-# NOTE: This example requires some huge 3D input files, 
-#       which are not included in OpenDrift repository. 
-#       Contact Knut-Frode (knutfd@met.no) if you would like
-#       access the data to run this example (~5 GB)
-################################################################
+
+# Copy the following files (~5 GB) to your local computer for faster run:
+# http://super-monitor.met.no/thredds/fileServer/lustreMntB/users/knutfd/public/AROME_MetCoOp_00_DEF.nc
+# http://super-monitor.met.no/thredds/fileServer/lustreMntB/users/knutfd/public/subfolder_test/NorKyst-800m_ZDEPTHS_his_00.nc
 
 o = OpenOil3D(loglevel=0)  # Set loglevel to 0 for debug information
 
 ncfile = 'oil3Dmixing.nc'
 import_file = False
 
+arome_file = '/disk1/data/opendrift_testdata/15jan2016/AROME_MetCoOp_00_DEF.nc'
+norkyst_file = '/disk1/data/opendrift_testdata/15jan2016/NorKyst-800m_ZDEPTHS_his_00.nc'
+if not os.path.isfile(arome_file):
+    arome_file = 'http://super-monitor.met.no/thredds/dodsC/lustreMntB/users/knutfd/public/AROME_MetCoOp_00_DEF.nc'
+    norkyst_file = 'http://super-monitor.met.no/thredds/dodsC/lustreMntB/users/knutfd/public/subfolder_test/NorKyst-800m_ZDEPTHS_his_00.nc'
+
 if import_file is True:
     o.io_import_file(ncfile)
 else:
-    # Arome
-    #reader_arome = reader_netCDF_CF_generic.Reader('/disk2/data/opendrift_test_data/15jan2016/AROME_MetCoOp_00_DEF.nc')
-    reader_arome = reader_netCDF_CF_generic.Reader('/disk1/data/opendrift_testdata/15jan2016/AROME_MetCoOp_00_DEF.nc')
-    #reader_arome = reader_netCDF_CF_generic.Reader('/opdata/arome2_5_main/AROME_MetCoOp_00_DEF.nc')
-
-    # Norkyst
-    #reader_norkyst = reader_netCDF_CF_generic.Reader('/disk2/data/opendrift_test_data/15jan2016/NorKyst-800m_ZDEPTHS_his_00.nc')
-    reader_norkyst = reader_netCDF_CF_generic.Reader('/disk1/data/opendrift_testdata/15jan2016/NorKyst-800m_ZDEPTHS_his_00.nc')
-    #reader_norkyst = reader_netCDF_CF_generic.Reader('/opdata/roms/NorKyst-800m_ZDEPTHS_his_00.nc')
+    reader_arome = reader_netCDF_CF_generic.Reader(arome_file)
+    reader_norkyst = reader_netCDF_CF_generic.Reader(norkyst_file)
 
     # Landmask (Basemap)
-    # Stad
     reader_basemap = reader_basemap_landmask.Reader(
                         llcrnrlon=3.0, llcrnrlat=61.4,
                         urcrnrlon=6.2, urcrnrlat=62.6,
@@ -46,10 +43,7 @@ else:
     #o.fallback_values['y_wind'] = 20
 
     # Seeding some particles
-    #lon = 5.0; lat = 60.0; # Outside Bergen
-    #lon = 5.15; lat = 58.85; # Outside Stavanger
     lon = 4.9; lat = 62.1; # Stad
-    #lon = 13.8; lat = 67.57; # Vestfjorden
 
     #time = [reader_arome.start_time,
     #        reader_arome.start_time + timedelta(hours=30)]
