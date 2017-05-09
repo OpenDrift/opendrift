824a8019eb066e0d57b598f0be3cd12b7769bb65
knutfrode <knutfrode@gmail.com>
2017-05-09 12:43:07 +0200

    Updated Travis configuration

--- opendrift/models/basemodel.py
+++ opendrift/models/basemodel.py
@@ -159,6 +159,7 @@ class OpenDriftSimulation(PhysicsMethods):
             self.fallback_values = {}
 
         # Make copies of dictionaries so that they are private to each instance
+        self.status_categories = self.status_categories.copy()
         self.fallback_values = self.fallback_values.copy()
         self.status_colors_default = self.status_colors_default.copy()
 
