983c70e1d2c0b4baa0f1931238565b86530bd235
simon-msl <simon@metocean.co.nz>
2018-04-12 11:07:36 +1200

    adding simple scrit to plot oil budget

---
+++ opendrift/scripts/opendrift_plot_oil_budget.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# This file is part of OpenDrift.
+# 
+# OpenDrift is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2
+# 
+# OpenDrift is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with OpenDrift.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Copyright 2015, Knut-Frode Dagestad, MET Norway
+
+
+#
+# wrapper to plot oil budget from netcdf file output from OpenDrfit oil spill simulation 
+#
+
+import sys
+import argparse
+import numpy as np
+sys.path.append("..")
+
+import opendrift
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',
+                        help='<OpenDrift output filename (netCDF)>')
+    parser.add_argument('-b', dest='buffer',
+                        default=0.1,
+                        help='Buffer around plot in degrees lon/lat.')
+
+    args = parser.parse_args()
+
+
+    o = opendrift.open(args.filename)
+    print o
+    o.plot_oil_budget()
