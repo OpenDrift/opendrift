90536730c85a8f1530a39775bde9f95e1e81bc27
knutfrode <knutfrode@gmail.com>
2018-12-06 14:39:40 +0100

    Fixed minor bug in OceanDrift3D, should be completed later

--- opendrift/models/oceandrift3D.py
+++ opendrift/models/oceandrift3D.py
@@ -86,6 +86,8 @@ class OceanDrift3D(OpenDrift3DSimulation, OceanDrift):
         to be used in turbulent mixing module.
         Using zero for passive particles, i.e. following water particles
         '''
+        
+        # TODO: setting to scalar has lead to trouble - should be array of num_elements
         self.elements.terminal_velocity = 0.
 
     def update(self):
@@ -102,8 +104,9 @@ class OceanDrift3D(OpenDrift3DSimulation, OceanDrift):
         self.stokes_drift()
 
         # Turbulent Mixing
-        self.update_terminal_velocity()
-        self.vertical_mixing()
+        if self.get_config('processes:turbulentmixing') is True:
+            self.update_terminal_velocity()
+            self.vertical_mixing()
 
         # Vertical advection
         self.vertical_advection()
