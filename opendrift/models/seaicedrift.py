5c0b156e060397124a94c1a33ecb3c99c0b32161
Knut-Frode Dagestad <knutfrode@gmail.com>
2019-01-28 08:14:36 +0100

    Added new module for advection with sea ice

---
+++ opendrift/models/seaicedrift.py
@@ -0,0 +1,43 @@
+# This file is part of OpenDrift.
+#
+# OpenDrift is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2
+#
+# OpenDrift is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenDrift.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2019, Knut-Frode Dagestad, MET Norway
+
+from opendrift.models.basemodel import OpenDriftSimulation
+from opendrift.elements.passivetracer import PassiveTracer
+
+
+class SeaIceDrift(OpenDriftSimulation):
+    """Trajectory model based on the OpenDrift framework.
+
+    Simply propagation with ocean sea ice (this module is not for ice bergs)
+    Developed at MET Norway.
+
+    """
+
+    ElementType = PassiveTracer
+    required_variables = ['sea_ice_x_velocity', 'sea_ice_y_velocity',
+                          'land_binary_mask']
+
+
+    def __init__(self, *args, **kwargs):
+
+        super(SeaIceDrift, self).__init__(*args, **kwargs)
+
+
+    def update(self):
+        """Update positions and properties of elements."""
+
+        # Move particles with sea ice velocity
+        self.advect_with_sea_ice()
