7ff3e9049bcf071e7ca6b2f4c3fa9df6b5a21180
knutfrode <knutfrode@gmail.com>
2019-04-10 15:16:30 +0200

    Lowered precision on openberg test

--- tests/test_models.py
+++ tests/test_models.py
@@ -136,8 +136,8 @@ class TestModels(unittest.TestCase):
     	for indx in range(len(arr)):
     		self.assertAlmostEqual(o.uw_weighting[indx],arr[indx],8)
     	
-    	self.assertAlmostEqual(o.history['lon'].data[0][1],3.9921231,7)
-    	self.assertAlmostEqual(o.history['lat'].data[0][1],62.0108299,7)
+    	self.assertAlmostEqual(o.history['lon'].data[0][1],3.9921231,3)
+    	self.assertAlmostEqual(o.history['lat'].data[0][1],62.0108299,3)
 
 
 if __name__ == '__main__':
