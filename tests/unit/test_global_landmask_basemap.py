f2546a7919e40b807dff119e0d4860234eae5608
Gaute Hope <gaute.hope@met.no>
2019-11-27 12:57:25 +0100

    Remove basemap tests

--- tests/unit/test_global_landmask_basemap.py
+++ tests/unit/test_global_landmask_basemap.py
@@ -1,16 +1,7 @@
 import numpy as np
 import pytest
 from . import *
-from opendrift.readers import reader_basemap_landmask
 from opendrift.readers import reader_global_landmask
-
-@pytest.mark.slow
-def test_basemap_setup(benchmark):
-    benchmark(
-        reader_basemap_landmask.Reader,
-            llcrnrlon=-1.5, llcrnrlat=59,
-            urcrnrlon=7, urcrnrlat=64
-        )
 
 def test_global_setup(benchmark):
     benchmark(reader_global_landmask.Reader)
@@ -36,25 +27,4 @@ def test_performance_global(benchmark):
 
     print ("masking against cartopy")
     benchmark(reader_global.__on_land__, xx,yy)
-
-@pytest.mark.veryslow
-def test_performance_basemap(benchmark):
-    print("setting up basemap")
-    reader_basemap = reader_basemap_landmask.Reader(
-                        llcrnrlon=18.64, llcrnrlat=69.537,
-                        urcrnrlon=19.37, urcrnrlat=69.81, resolution = 'f')
-
-
-    x = np.linspace(18.641, 19.369, 100)
-    y = np.linspace(69.538, 69.80, 100)
-
-    xx, yy = np.meshgrid(x,y)
-    xx = xx.ravel()
-    yy = yy.ravel()
-
-    # warmup
-    reader_basemap.__on_land__(xx, yy)
-
-    print ("masking against basemap")
-    benchmark(reader_basemap.__on_land__, xx, yy)
 
