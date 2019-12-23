efe8c761aac3399801c97aac2c6c50526b9020ba
magnes <magnes@met.no>
2019-12-23 13:40:00 +0100

    Add write to concentration netcdf file in postprocessing step in example script

--- examples/example_radionuclides.py
+++ examples/example_radionuclides.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
 """
 Radionuclides
 =============
 """
+#!/usr/bin/env python
+
 
 from opendrift.readers import reader_netCDF_CF_generic, reader_ROMS_native
 from opendrift.models.radionuclides import RadionuclideDrift
@@ -11,26 +12,38 @@ import numpy as np
 from numpy import dtype
 
 
-o = RadionuclideDrift(loglevel=20, seed=0)  # Set loglevel to 0 for debug information
+
+
+o = RadionuclideDrift(
+                      loglevel=20,seed=0)  # Set loglevel to 0 for debug information
+
+# Arome
+#reader_arome = reader_netCDF_CF_generic.Reader(o.test_data_folder() + '16Nov2015_NorKyst_z_surface/arome_subset_16Nov2015.nc')
+#reader_arome = reader_netCDF_CF_generic.Reader('http://thredds.met.no/thredds/dodsC/meps25files/meps_det_extracted_2_5km_latest.nc')
 
 # Norkyst
 #reader_norkyst = reader_netCDF_CF_generic.Reader(o.test_data_folder() + '/14Jan2016_NorKyst_z_3d/NorKyst-800m_ZDEPTHS_his_00_3Dsubset.nc')
-reader_norkyst = reader_netCDF_CF_generic.Reader('https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
+reader_norkyst = reader_netCDF_CF_generic.Reader('http://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
 
+#o.add_reader([reader_norkyst, reader_arome])
 o.add_reader([reader_norkyst])
 
+#time = reader_norkyst.start_time
 td=datetime.today()
-time = datetime(td.year, td.month, td.day, 0)
+time = datetime(td.year, td.month, td.day,0)
 
 #latseed= 61.2; lonseed= 4.3    # Sognesjen
 latseed= 59.0;   lonseed= 10.75 # Hvaler/Koster
 
-ntraj=1000
-iniz=np.random.rand(ntraj) * -10. # seeding the radionuclides in the upper 10m
+
+
+ntraj=2000
+iniz=np.random.rand(ntraj) * -10.         # seeding the radionuclides in the upper 10m
 init_speciation = np.ones(ntraj)*0
 diam=np.zeros_like(init_speciation,dtype=np.float32)
 o.seed_elements(lonseed, latseed, z=iniz, radius=100,number=ntraj,
-                time=time, diameter=diam, specie=init_speciation)
+                time=time,
+                diameter=diam, specie=init_speciation)
 
 init_speciation = np.ones(ntraj)*1
 diam=np.zeros_like(init_speciation,dtype=np.float32) * 5.e-6
@@ -44,14 +57,12 @@ o.seed_elements(lonseed, latseed, z=iniz, radius=100,number=ntraj,
 #                 diameter=diam, specie=init_speciation)
 
 # Adjusting some configuration
-o.set_config('drift:vertical_mixing', False)
-#o.set_config('environment:fallback:ocean_vertical_diffusivity', 0)
-#o.set_config('vertical_mixing:diffusivitymodel','constant')  # include settling without vertical turbulent mixing
-#o.set_config('vertical_mixing:diffusivitymodel','environment')  # include settling without vertical turbulent mixing
+o.set_config('processes:turbulentmixing', True)
+o.set_config('turbulentmixing:diffusivitymodel','zero')  # include settling without vertical turbulent mixing
+#o.set_config('turbulentmixing:diffusivitymodel','environment')  # include settling without vertical turbulent mixing
 # Vertical mixing requires fast time step
-o.set_config('vertical_mixing:timestep', 600.) # seconds
+o.set_config('turbulentmixing:timestep', 600.) # seconds
 
-#%%
 # Activate the desired species
 o.set_config('radionuclide:species:LMM', True)
 #o.set_config('radionuclide:species:LMMcation', True)
@@ -89,12 +100,10 @@ o.set_config('general:coastline_action', 'previous')
 
 o.list_configspec()
 
-#%%
-# Running model
-o.run(steps=48*2, time_step=1800, time_step_output=3600)
+# Running model (until end of driver data)
+o.run(steps=48*2, time_step=1800,outfile='radio.nc')
 
 
-#%%
 # Print and plot results
 print(o)
 print('Final speciation:')
@@ -102,47 +111,36 @@ for isp,sp in enumerate(o.name_species):
     print ('{:32}: {:>6}'.format(sp,sum(o.elements.specie==isp)))
 
 print('Number of transformations:')
-for isp in range(o.nspecies):
-    print('{}'.format(['{:>9}'.format(np.int(item)) for item in o.ntransformations[isp,:]]) )
+print (o.ntransformations)
+
+
+# Postprocessing: write to concentration netcdf file 
+o.conc_lat   = reader_norkyst.lat
+o.conc_lon   = reader_norkyst.lon
+o.conc_topo  = reader_norkyst.get_variables('sea_floor_depth_below_sea_level',
+                                       x=[reader_norkyst.xmin,reader_norkyst.xmax], 
+                                       y=[reader_norkyst.ymin,reader_norkyst.ymax], block=True)['sea_floor_depth_below_sea_level'][:]
+o.write_netcdf_radionuclide_density_map('conc.nc', pixelsize_m=600, deltaz=-5)
+
 
 o.animation(color='specie',
             vmin=0,vmax=o.nspecies-1,
             colorbar=True,
-            fast = True
+            filename='radionuclides.gif'
+#            fast = True
             )
 #%%
-# .. image:: /gallery/animations/example_radionuclides_0.gif
+# .. image:: /gallery/animations/radionuclides.gif
 
 #o.plot_vertical_distribution()
 #o.plot_property('specie')
-o.animation_profile()
+o.animation_profile(filename='radionuclides_profile.gif')
 #%%
-# .. image:: /gallery/animations/example_radionuclides_1.gif
+# .. image:: /gallery/animations/radionuclides_profile.gif
 
-o.plot(linecolor='specie',vmin=0,vmax=o.nspecies-1,fast=True,)
+#o.plot(linecolor='specie',vmin=0,vmax=o.nspecies-1,#fast=True,
+#       background='land_binary_mask',
+       #background='sea_floor_depth_below_sea_level'
+#       )
 
-
-
-# Postprocessing: write to concentration netcdf file
-o.conc_lat   = reader_norkyst.lat
-o.conc_lon   = reader_norkyst.lon
-o.conc_topo  = reader_norkyst.get_variables('sea_floor_depth_below_sea_level',
-                                       x=[reader_norkyst.xmin,reader_norkyst.xmax],
-                                       y=[reader_norkyst.ymin,reader_norkyst.ymax], block=True)['sea_floor_depth_below_sea_level'][:]
-#o.conc_mask  = reader_norkyst.land_binary_mask
-
-#%%
-#
-# .. code:
-#
-#  o.write_netcdf_radionuclide_density_map('radio_conc.nc', pixelsize_m=200.,
-#                                        zlevels=[-2.],
-#                                        activity_unit='Bq',
-#                                        horizontal_smoothing=True,
-#                                        smoothing_cells=1,
-#                                        time_avg_conc=True,
-#                                        deltat=2., # hours
-#                                        llcrnrlon=8.9681, llcrnrlat=58.6627,
-#                                        urcrnrlon=9.2772, urcrnrlat=58.7219,
-#                                        )
 
