be212897bcfd2de038cbdc5d1b31149aa092d9fe
knutfrode <knutfrode@gmail.com>
2020-01-21 14:53:23 +0100

    Made two example scripts Python3 compatible

--- examples/example_long_double_gyre_LCS.py
+++ examples/example_long_double_gyre_LCS.py
@@ -20,7 +20,7 @@ o.fallback_values['land_binary_mask'] = 0
 o.set_config('drift:scheme', 'runge-kutta4')
 
 double_gyre = reader_double_gyre.Reader(epsilon=.25, omega=0.628, A=0.1)
-print double_gyre
+print(double_gyre)
 
 o.add_reader(double_gyre)
 
