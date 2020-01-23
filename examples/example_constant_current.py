6ba89af20e9f4a36423cfdbfae84236746071e6c
knutfrode <knutfrode@gmail.com>
2020-01-23 10:17:07 +0100

    Updated example_constant_current

--- examples/example_constant_current.py
+++ examples/example_constant_current.py
@@ -7,7 +7,7 @@ Constant current
 from datetime import datetime, timedelta
 from opendrift.models.oceandrift import OceanDrift
 
-o = OceanDrift(loglevel=0)  # Set loglevel to 0 for debug information
+o = OceanDrift(loglevel=20)  # Set loglevel to 0 for debug information
 
 # Adding no input models, but instead constant northwards current of 1 m/s
 o.fallback_values['x_sea_water_velocity'] = 0
@@ -15,7 +15,7 @@ o.fallback_values['y_sea_water_velocity'] = 1
 o.fallback_values['land_binary_mask'] = 0
 
 # Seed elements at defined position and time
-o.seed_elements(lon=4.3, lat=60.0, radius=5000, number=100,
+o.seed_elements(lon=4.0, lat=60.0, radius=5000, number=100,
                 time=datetime(2015, 9, 22, 6, 0, 0))
 
 # Running model for 50 hours
