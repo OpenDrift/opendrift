d6cfd18815cf9d3b107bb36f831f1c663a81adc1
knutfrode <knutfrode@gmail.com>
2020-01-23 10:47:10 +0100

    Commenting some example scripts

--- examples/example_roms_native.py
+++ examples/example_roms_native.py
@@ -10,19 +10,22 @@ from opendrift.models.oceandrift import OceanDrift
 
 o = OceanDrift(loglevel=0)  # Set loglevel to 0 for debug information
 
-# Nordic 4km
+#%%
+# Creating and adding reader for Nordic 4km current dataset
 nordic_native = reader_ROMS_native.Reader(o.test_data_folder() +
     '2Feb2016_Nordic_sigma_3d/Nordic-4km_SLEVELS_avg_00_subset2Feb2016.nc')
-
 o.add_reader(nordic_native)
 
-# Seed elements at defined position and time
+#%%
+# Seed elements at defined positions, depth and time
 o.seed_elements(lon=12.0, lat=68.3, radius=0, number=10,
                 z=np.linspace(0, -150, 10), time=nordic_native.start_time)
 
+#%%
 # Running model
 o.run(time_step=3600)
 
+#%%
 # Print and plot results
 print(o)
 o.plot(linecolor='z', fast=True)
