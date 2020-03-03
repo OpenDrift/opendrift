4ef41613c41299bf84bb89c259355cb8f84f08a0
magnes <magnes@met.no>
2020-03-03 09:08:34 +0100

    Include concentration output in example script
    Fix typo

--- examples/example_radionuclides.py
+++ examples/example_radionuclides.py
@@ -101,7 +101,8 @@ for isp,sp in enumerate(o.name_species):
     print ('{:32}: {:>6}'.format(sp,sum(o.elements.specie==isp)))
 
 print('Number of transformations:')
-print (o.ntransformations)
+for isp in range(o.nspecies):
+    print('{}'.format(['{:>9}'.format(np.int(item)) for item in o.ntransformations[isp,:]]) )
 
 o.animation(color='specie',
             vmin=0,vmax=o.nspecies-1,
@@ -122,4 +123,24 @@ o.plot(linecolor='specie',vmin=0,vmax=o.nspecies-1,fast=True,
        #background='sea_floor_depth_below_sea_level'
        )
 
+
+
+# Postprocessing: write to concentration netcdf file 
+o.conc_lat   = reader_norkyst.lat
+o.conc_lon   = reader_norkyst.lon
+o.conc_topo  = reader_norkyst.get_variables('sea_floor_depth_below_sea_level',
+                                       x=[reader_norkyst.xmin,reader_norkyst.xmax], 
+                                       y=[reader_norkyst.ymin,reader_norkyst.ymax], block=True)['sea_floor_depth_below_sea_level'][:]
+#o.conc_mask  = reader_norkyst.land_binary_mask
+
+o.write_netcdf_radionuclide_density_map('radio_conc.nc', pixelsize_m=200., 
+                                        zlevels=[-2.],
+                                        activity_unit='Bq',
+                                        horizontal_smoothing=True,
+                                        smoothing_cells=1,
+#                                        time_avg_conc=True,
+#                                        deltat=2., # hours
+#                                        llcrnrlon=8.9681, llcrnrlat=58.6627,
+#                                        urcrnrlon=9.2772, urcrnrlat=58.7219,
+                                        )
 
