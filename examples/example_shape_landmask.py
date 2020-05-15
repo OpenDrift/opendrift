e4dfae3fe50f47efb5b2a1e8db2ae6f9e8ebf016
Gaute Hope <gaute.hope@met.no>
2020-05-15 13:26:19 +0200

    reader_shape: read land shapes from shapefiles

---
+++ examples/example_shape_landmask.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+"""
+Use a shapefile as landmask
+===========================
+"""
+
+import numpy as np
+from opendrift.readers import reader_netCDF_CF_generic
+from opendrift.readers import reader_ROMS_native
+from opendrift.readers import reader_shape
+from opendrift.models.oceandrift import OceanDrift
+
+o = OceanDrift(loglevel=0)  # Set loglevel to 0 for debug information
+o.max_speed = 3
+
+# This example works better using hourly input from Thredds than the daily data from test folder
+reader_nordic = reader_netCDF_CF_generic.Reader('https://thredds.met.no/thredds/dodsC/sea/nordic4km/zdepths1h/aggregate_be')
+
+#%%
+# Use shapes from Cartopy for tests. These shapefiles are less acurate than those
+# provided by the GSHHS dataset (available though the reader_global_landmask reader).
+import cartopy.io.shapereader as shpreader
+shpfilename = shpreader.natural_earth(resolution='110m',
+                                    category='cultural',
+                                    name='admin_0_countries')
+reader_natural = reader_shape.Reader(shpfilename)
+#reader_nordic = reader_ROMS_native.Reader(o.test_data_folder() +
+#    '2Feb2016_Nordic_sigma_3d/Nordic-4km_SLEVELS_avg_00_subset2Feb2016.nc')
+
+o.add_reader([reader_nordic, reader_natural])
+o.set_config('general:use_auto_landmask', False)
+o.set_config('general:coastline_action', 'stranding')
+
+#%%
+# Seeding elements on a grid
+lons = np.linspace(12, 14.5, 30)
+lats = np.linspace(67.5, 68.5, 30)
+lons, lats = np.meshgrid(lons, lats)
+lon = lons.ravel()
+lat = lats.ravel()
+
+time = reader_nordic.start_time
+o.seed_elements(lon, lat, radius=0, number=30*30, time=time)
+
+o.run(steps=24*2, time_step=3600)
+
+#%%
+# Print and plot results
+print(o)
+# o.plot()
+from matplotlib.colors import ListedColormap
+import cartopy.feature as cfeature
+cmap = ListedColormap((cfeature.COLORS['water'],
+                       cfeature.COLORS['land']))
+o.plot(background='land_binary_mask', hide_landmask=True, cmap=cmap)
+#o.plot(background=['x_sea_water_velocity', 'y_sea_water_velocity'])
+o.animation()
+
+#%%
+# .. image:: /gallery/animations/example_shape_landmask_0.gif
---
+++ opendrift/readers/reader_shape.py
@@ -0,0 +1,100 @@
+# This file is part of OpenDrift.
+#
+# OpenDrift is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2
+#
+# OpenDrift is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenDrift.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright 2020, Gaute Hope, MET Norway
+
+from opendrift.readers.basereader import BaseReader
+from opendrift_landmask_data import Landmask
+
+import pyproj
+import shapely
+import shapely.ops
+import cartopy
+
+class Reader(BaseReader):
+    """
+    The shape-file reader (.shp) can be used to load generic shape files the 'landmask' variable.
+
+    Args:
+        :param shapes: shape-file or files (.shp)
+        :type shapes: string or list of strings.
+
+        :param proj4_str: Proj.4 string of shape file projection coordinates
+                          (default: '+proj=lonlat +ellps=WGS84').
+        :type proj4_str: string.
+    """
+    name = 'shape'
+    return_block = False
+    variables = ['land_binary_mask']
+    proj4 = None
+    crs   = None
+    land  = None
+    shapes = None
+    always_valid = True
+
+    def __init__(self, shapes, proj4_str = '+proj=lonlat +ellps=WGS84'):
+        self.proj4 = proj4_str
+        self.crs = pyproj.CRS(self.proj4)
+        if isinstance(shapes, list):
+            self.shapes = shapes
+        else:
+            self.shapes = [ shapes ]
+
+        super (Reader, self).__init__ ()
+
+        # Depth
+        self.z = None
+
+        # reading shapefiles
+        land = []
+        for shp in self.shapes:
+            self.logger.debug("Reading shapefile: %s" % shp)
+            from cartopy import io
+            reader = io.shapereader.Reader(shp)
+            land.extend(reader.geometries())
+
+        assert len(land) > 0, "no geometries loaded"
+
+        self.logger.info("Pre-processing %d geometries" % len(land))
+        self.land = shapely.ops.unary_union(land)
+        del land
+
+        self.xmin, self.ymin, self.xmax, self.ymax = self.land.bounds
+        self.xmin, self.ymin = self.lonlat2xy(self.xmin, self.ymin)
+        self.xmax, self.ymax = self.lonlat2xy(self.xmax, self.ymax)
+
+    def __on_land__(self, x, y):
+        return shapely.vectorized.contains(self.land, x, y)
+
+    def get_variables(self, requestedVariables, time = None,
+                      x = None, y = None, z = None, block = False):
+        """
+        Get binary mask of whether elements are on land or not.
+
+        Args:
+            x (deg[]): longitude (decimal degrees)
+            y (deg[]): latitude (decimal degrees)
+            ...
+
+        x, y is given in reader local projection.
+
+        Returns:
+            Binary mask of point x, y on land.
+
+        """
+
+        self.check_arguments(requestedVariables, time, x, y, z)
+        return { 'x' : x, 'y' : y, 'land_binary_mask': self.__on_land__(x,y) }
+
+
