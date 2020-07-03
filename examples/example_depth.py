a26f9073977e6b4c2d662e152bd849db16c89a3d
Knut-Frode Dagestad <knutfrode@gmail.com>
2020-07-03 13:39:12 +0200

    Trigger rebuild of examples with version 1.3.1

--- examples/example_depth.py
+++ examples/example_depth.py
@@ -11,30 +11,34 @@ from opendrift.models.oceandrift import OceanDrift
 
 o = OceanDrift(loglevel=20)  # Set loglevel to 0 for debug information
 
-#%% Using live data from Thredds
+#%%
+# Using live data from Thredds
 o.add_readers_from_list([
     'https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be'])
 
-#%% Seed elements at defined position and time
-z = -np.random.rand(1000)*50  # Giving elements a random depth
+#%%
+# Seed 1000 elements at random depths
+z = -np.random.rand(1000)*50
 o.seed_elements(lon=4.8, lat=60.0, z=z, radius=0, number=1000,
                 time=datetime.now())
 
 print(o)
 
-#%% Adding some diffusion
+#%%
+# Adding some diffusion
 o.set_config('drift:current_uncertainty', .1)
 o.set_config('drift:wind_uncertainty', 2)
 
-#%% Running model
+#%%
+# Running model
 o.run(duration=timedelta(hours=24), time_step=1800, outfile='openoil.nc',
       export_buffer_length=5)  # Writing to netCDF file every 5 time steps
 
-#%% Plot results with lines and particles colored by depth
+#%%
+# Plot results with lines and particles colored by depth
 print(o)
 o.plot(linecolor='z', buffer=.1, show_particles=False, fast=True)
 o.animation(color='z', buffer=.1, fast=True)
-
 
 #%%
 # .. image:: /gallery/animations/example_depth_0.gif
