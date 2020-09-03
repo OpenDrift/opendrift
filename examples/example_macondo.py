d0eaffddbc646f6c56122e20bf4b5606cea2c102
knutfrode <knutfrode@gmail.com>
2020-09-03 11:32:30 +0200

    reader_netCDF_CF_generic now parses time as strings, and user may provide mapping of standard_name, if not given in netCDF file. Also updated example_macondo.py, to be shown in gallery.

--- examples/example_macondo.py
+++ examples/example_macondo.py
@@ -8,16 +8,19 @@ from datetime import datetime, timedelta
 from opendrift.readers import reader_netCDF_CF_generic
 from opendrift.models.openoil import OpenOil
 
+#%%
 # This is a highly simplified 2D simulation, for illustration purpose only
 
 o = OpenOil(loglevel=20)  # Set loglevel to 0 for debug information
 
-#reader_hycom = reader_netCDF_CF_generic.Reader('https://tds.hycom.org/thredds/dodsC/GLBu0.08/expt_19.1/2010/3hrly')
-#print(reader_hycom0)
-reader_globcurrent = reader_netCDF_CF_generic.Reader('https://tds0.ifremer.fr/thredds/dodsC/CLS-L4-CUREUL_HS-ALT_SUM-V02.0_FULL_TIME_SERIE')  # Total
+#%%
+# For this datasource which does not contain standard_name, we impose a variable mapping
+reader_globcurrent = reader_netCDF_CF_generic.Reader(
+    'http://tds0.ifremer.fr/thredds/dodsC/GLOBCURRENT-L4-CUREUL_HS-ALT_SUM-V03.0',
+    standard_name_mapping={'eastward_eulerian_current_velocity': 'x_sea_water_velocity',
+                           'northward_eulerian_current_velocity': 'y_sea_water_velocity'})
 
 reader_oceanwind = reader_netCDF_CF_generic.Reader('https://tds0.ifremer.fr/thredds/dodsC/CERSAT-GLO-CLIM_WIND_L4-OBS_FULL_TIME_SERIE')
-#print(reader_oceanwind)
 
 # Add readers
 o.add_reader([reader_globcurrent, reader_oceanwind])
@@ -25,19 +28,20 @@ o.add_reader([reader_globcurrent, reader_oceanwind])
 # Seed some particles
 lon = -88.387161; lat = 28.736669  # Macondo location
 starttime = datetime(2010, 4, 21, 6, 0, 0)  # 4 hours after explosion
-time = [starttime, starttime + timedelta(hours=24*40)]
-o.seed_elements(lon, lat, radius=0, number=5000, time=time)
+time = [starttime, starttime + timedelta(hours=24*30)]
+o.seed_elements(lon, lat, radius=0, number=15000, time=time, oiltype='LIGHT LOUISIANNA SWEET, BP')
 
 # Run model
 print(o)
-o.run(duration=timedelta(days=40),
+o.run(duration=timedelta(days=30),
       time_step=timedelta(hours=3),
-      time_step_output=timedelta(days=1))
+      time_step_output=timedelta(hours=6))
 
 # Print and plot results
 print(o)
 o.plot(fast=True)
-o.animation(filename='macondo.gif', fast=True)
+o.plot_oil_budget()
+o.animation(fast=True)
 
 #%%
 # .. image:: /gallery/animations/example_macondo_0.gif
