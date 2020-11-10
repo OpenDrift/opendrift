fad3f89916274d0f89bf6a677fd799c18f8e24b5
Knut-Frode Dagestad <knutfrode@gmail.com>
2020-11-11 00:11:38 +0100

    Update model_template.py

--- opendrift/models/model_template.py
+++ opendrift/models/model_template.py
@@ -43,6 +43,16 @@ class TemplateElementType(Lagrangian3DArray):
     These are added to the inherited properties (e.g. from OceanDrift)
     Property names may be freely chosen, but the "update" function (below)
     will refer to the names in the specifications of the processes.
+    
+    The "default" value is used for alle elements, unless a custom
+    value is provided at time of seeding:
+    o.seed_elements(......., <property>=<value>)
+    
+    The default value can also be updated by the user
+    before seeding with the config mechanism:
+    o.set_config('seed:<property>', <new default value>)
+    
+    The "units" attribute is used for labeling.
     """
     variables = Lagrangian3DArray.add_variables([
         ('new_property_1', {'dtype': np.float32,
@@ -62,7 +72,7 @@ class ModelTemplate(OceanDrift):
     """
 
     # Specify which element/particle type (class) to be used by this model.
-    # Presently, a model can have/use only one element type
+    # A model can have/use only one element type
     ElementType = TemplateElementType
 
     # Specify which environment variables (e.g. wind, waves, currents...)
@@ -75,30 +85,84 @@ class ModelTemplate(OceanDrift):
     required_variables = {
         'x_sea_water_velocity': {'fallback': 0},
         'y_sea_water_velocity': {'fallback': 0},
-        'x_wind': {'fallback': 0},
-        'y_wind': {'fallback': 0},
+        'x_wind': {'fallback': 0, 'important': False},
+        'y_wind': {'fallback': 0, 'important': False},
+        'ocean_vertical_diffusivity': {'fallback': 0.02, 'important': False, 'profiles': True}
         'land_binary_mask': {'fallback': None},
         }
-    # Default (fallback) numerical values may be specified.
-    # These values will be used for particles outside of the
-    # spatial or tempoaral domain of all the available readers,
+    # Default (fallback) numerical values will be used for particles 
+    # outside of the spatial or tempoaral domain of all the available readers,
     # or if no readers are given for the specific variables.
-
+    # If the fallback value is None, this means that simulation will
+    # not run unless a reader has been added to provide this variable.
+    # The attribute *important' relates to the concept of Lazy readers:
+    # https://opendrift.github.io/tutorial.html#lazy-readers
+    # Lazy readers will be initialized successively until there are active
+    # readers for all variables, except for variables which are
+    # marked as not important, as for wind in the example above.
+    # If the attribute 'profiles' is True for a variable (as for
+    # ocean_vertical_diffusivity in the example above), a vertical profile
+    # of this variable is obtained over the vertical depth interval as 
+    # defined below. These profiles are used for the vertical turbulence scheme:
+    # https://opendrift.github.io/_modules/opendrift/models/oceandrift.html#OceanDrift.vertical_mixing
+    # Profiles are typically required for ocean_vertical_diffusivity, and sometimes for 
+    # salinity and temperature in order to calculate the stratification.
+    required_profiles_z_range = [-50, 0]
+    
     def __init__(self, *args, **kwargs):
-        """
 
-        Here any preprocessing and initialisation of a simulation
-        may be performed, e.g. importing external data from file.
+       # Here any preprocessing and initialisation of a simulation
+       # may be performed, e.g. importing external data from file.
 
-        .. code::
-
-            print('preprocessing...')
-
-        Constructor of parent class must always be called
-        to perform some necessary common initialisation tasks
-
-        .. code::
-            super(ModelTemplate, self).__init__(*args, **kwargs)
+       # The constructor of parent class must always be called
+       # to perform some necessary common initialisation tasks:
+       super(ModelTemplate, self).__init__(*args, **kwargs)
+            
+       # The above initialisation includes adding common config options.
+       # Further config options specific to this subclass can be
+       # added with the following method:
+       self._add_config({
+            'seed:m3_per_hour': {'type': 'float', 'default': 1,
+                'min': 0, 'max': 1e10, 'units': 'm3 per hour',
+                'description': 'The amount (volume) of oil released per hour (or total amount if release is instantaneous)',
+                'level': self.CONFIG_LEVEL_ESSENTIAL},
+            'seed:droplet_diameter_min_subsea': {'type': 'float', 'default': 0.0005,
+                'min': 1e-8, 'max': 1, 'units': 'meters',
+                'description': 'The minimum dimaeter of oil droplet for a subsea release.',
+                'level': self.CONFIG_LEVEL_BASIC},
+            'seed:droplet_diameter_max_subsea': {'type': 'float', 'default': 0.005,
+                'min': 1e-8, 'max': 1, 'units': 'meters',
+                'description': 'The maximum dimaeter of oil droplet for a subsea release.',
+                'level': self.CONFIG_LEVEL_BASIC},
+            'processes:dispersion': {'type': 'bool', 'default': True,
+                'description': 'Oil is removed from simulation (dispersed), if entrained as very small droplets.',
+                'level': self.CONFIG_LEVEL_BASIC}
+           })
+        
+        # The _add_config method takes a dictionary where keys are config settings,
+        # which can be adjusted by the user by:
+        # >>> o.set_config(key, value)
+        # and whose actual value is obtained during the simulation (*update* method below) by
+        # value = o.get_config(key)
+        # The config keys have a colon (:) separator for logical grouping into categories.
+        # The config specification above has the following items:
+        # - type: one of ['float', 'int', 'bool', 'enum']
+        # - default: the default value
+        # - units: the unit, used for labeling (potentially later for automatic scaling)
+        # - description: a human readable help text, e.g. to be displayed in the GUI
+        # - level:
+        #   - CONFIG_LEVEL_ESSENTIAL (1): for config settings which the user must consider.
+        #        These config items are e.g. shown on the front/seeding page of a GUI
+        #   - CONFIG_LEVEL_BASIC (2): for config settings which most users may consider to change
+        #   - CONFIG_LEVEL_ADVANCED (3): for config setting which only advanced users might change
+        #
+        # - If 'type' is 'float' or 'int' there are two additional items:
+        #    - 'min' and 'max' which are the minimum and maximum allowed values of this setting.
+        #       The set_config() method will raise an error of provided values are outside of this range.
+        # - If 'type' is 'enum' there is an additional item 'enum' which is a list
+        #     of the allowed values. These may be strings or numbers.
+           
+        
         """
         super(ModelTemplate, self).__init__(*args, **kwargs)
 
