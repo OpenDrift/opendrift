b837ff5f034c65279952df3a2650f8d06f16c42e
Gaute Hope <gaute.hope@met.no>
2020-12-07 14:40:42 +0100

    grib2: return block

--- opendrift/readers/reader_grib2.py
+++ opendrift/readers/reader_grib2.py
@@ -17,15 +17,18 @@
 import logging
 logger = logging.getLogger(__name__)
 
+import numpy as np
 import xarray as xr
+from datetime import datetime
 
 from .basereader import BaseReader
 from .basereader.structured import StructuredReader
 
 class Reader(BaseReader, StructuredReader):
     dataset = None
+    variables = None
 
-    def __init__(self, filename, engine = 'cfgrib'):
+    def __init__(self, filename, proj4 = None, engine = 'cfgrib'):
         """
         Grib file reader
 
@@ -33,22 +36,123 @@ class Reader(BaseReader, StructuredReader):
 
             filename: path to grib or grib2 file.
 
+            proj4: Optional projection override.
+
             engine: grib engine used by xarray, default is `cfgrib`.
 
         Returns: Grib-file reader.
 
         """
         self.dataset = xr.open_dataset(filename, engine = engine)
+        self.name = filename
 
-        self.xmin = self.dataset.longitude.min()
-        self.xmax = self.dataset.longitude.max()
-        self.ymin = self.dataset.latitude.min()
-        self.ymax = self.dataset.latitude.max()
+        self.lat = self.dataset.latitude.values
+        self.lon = self.dataset.longitude.values
+        self.delta_x = np.abs(self.lon[0,1] - self.lon[0,0]).item()
+        print(self.delta_x)
+
+        self.xmin = self.dataset.longitude.min().item()
+        self.xmax = self.dataset.longitude.max().item()
+        self.ymin = self.dataset.latitude.min().item()
+        self.ymax = self.dataset.latitude.max().item()
+
+        self.times = self.dataset.time.values
+
+        # convert to python datetime
+        assert '[ns]' in self.times.dtype.str, "datetime is not in nanoseconds"
+        self.times = np.array([datetime.utcfromtimestamp(d.astype('int') / 1e9) for d in self.times])
+
+        self.start_time = self.times[0]
+        self.end_time = self.times[-1]
+        self.time_step = self.times[1] - self.times[0]
+        assert np.all(np.diff(self.times) == self.time_step), "time step not constant"
+
+        # scan for projection
+        self.proj4 = proj4
+        if self.proj4 is None:
+            for v in self.dataset.variables:
+                v = self.dataset[v]
+                if 'GRIB_gridType' in v.attrs:
+                    if self.proj4 is None:
+                        self.proj4 = v.attrs['GRIB_gridType']
+                        logger.info("projection: %s" % self.proj4)
+                    else:
+                        assert self.proj4 == v.attrs['GRIB_gridType'], "all variables must have the same projection"
+        else:
+            logger.info("Using supplied projection: %s" % self.proj4)
+
+        # parsing variables
+        self.variable_mapping = {}
+
+        for v in self.dataset.variables:
+            var = self.dataset[v]
+            std_name = var.attrs.get('GRIB_cfName', None)
+
+            # TODO
+            if std_name == 'eastward_wind':
+                std_name = 'x_wind'
+
+            if std_name is not None:
+                self.variable_mapping[std_name] = v
+                logger.debug("Found standard variable: %s" % v)
+
+        self.variables = list(self.variable_mapping.keys())
 
         super().__init__()
 
     def get_variables(self, requested_variables, time=None,
                       x=None, y=None, z=None):
-        pass
+
+        requested_variables, time, x, y, z, outside = self.check_arguments(
+            requested_variables, time, x, y, z)
+
+        nearestTime, dummy1, dummy2, indxTime, dummy3, dummy4 = \
+            self.nearest_time(time)
+
+        assert np.all(z == self.dataset.heightAboveGround.item()), "height does not match layer"
+
+        logger.debug("Request variables: %s" % requested_variables)
+
+        variables = {}
+
+        delta = 1.
+        lonmin = np.maximum(x.min() - delta, self.xmin)
+        lonmax = np.minimum(x.max() + delta, self.xmax)
+        latmin = np.maximum(y.min() - delta, self.ymin)
+        latmax = np.minimum(y.max() + delta, self.ymax)
+
+        W = np.argwhere(
+                np.logical_and(
+                    np.logical_and(self.lat>=latmin, self.lat<=latmax),
+                    np.logical_and(self.lon>=lonmin, self.lon<=lonmax)))
+
+        assert len(W) > 0, "no matching values"
+
+        ix0 = np.min(W[:,0])
+        ix1 = np.max(W[:,0])
+        iy0 = np.min(W[:,1])
+        iy1 = np.max(W[:,1])
+
+        print(ix0, ix1)
+        print(iy0, iy1)
+
+        print(lonmin, lonmax, latmin, latmax, W)
+        variables['y'] = self.lat[ix0:ix1, iy0:iy1]
+        variables['x'] = self.lon[ix0:ix1, iy0:iy1]
+
+        print(variables)
+
+        for v in requested_variables:
+            var = self.variable_aliases.get(v, v)
+            var = self.variable_mapping[v]
+            var = self.dataset[var]
+            logger.debug("Fetching %s [%d:%d, %d:%d]" % (v, ix0, ix1, iy0, iy1))
+            variables[v] = var[indxTime, 0, ix0:ix1, iy0:iy1].values
+
+        variables['time'] = nearestTime
+        variables['z'] = z
+
+        return variables
+
 
 
--- tests/unit/test_grib2.py
+++ tests/unit/test_grib2.py
@@ -7,10 +7,19 @@ from opendrift.models.oceandrift import OceanDrift
 
 caps_u10 = 'grb/u_vel.grb2'
 caps_v10 = 'grb/v_vel.grb2'
+proj4 = 'EPSG:3995'
 
 def test_open():
-    r = Grib(caps_u10)
+    r = Grib(caps_u10, proj4 = proj4)
     print(r)
-    # r.plot_mesh()
-    # plt.show()
+
+def test_get_variables():
+    r = Grib(caps_u10, proj4 = proj4)
+
+    x = [10]
+    y = [60]
+    z = [10]
+
+    u = r.get_variables(['x_wind'], r.start_time, x, y, z)['x_wind']
+    print(u)
 
