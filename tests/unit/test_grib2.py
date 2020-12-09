fd1905d92e633efc30747231e27588e8e24d596a
Gaute Hope <gaute.hope@met.no>
2020-12-09 14:03:27 +0100

    grb: found proj

--- tests/unit/test_grib2.py
+++ tests/unit/test_grib2.py
@@ -8,13 +8,11 @@ from opendrift.models.oceandrift import OceanDrift
 caps_u10 = 'grb/u_vel.grb2'
 caps_v10 = 'grb/v_vel.grb2'
 proj4 = 'EPSG:3995'
-proj4 = '+proj=stere +lat_0=90 +lat_ts=45 +lon_0=-170 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
-proj4 = 'fakeproj'
+proj4 = '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=264 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
 
 def test_open():
     r = Grib(caps_u10, proj4 = proj4)
-    assert r.projected is False
-    print(r)
+    assert r.projected is True
 
 def test_get_variables():
     r = Grib(caps_u10, proj4 = proj4)
@@ -22,6 +20,9 @@ def test_get_variables():
     x = [10]
     y = [60]
     z = [10]
+
+    x, y = r.lonlat2xy(x, y)
+    print(x, y)
 
     u = r.get_variables(['x_wind'], r.start_time, x, y, z)['x_wind']
     print(u)
