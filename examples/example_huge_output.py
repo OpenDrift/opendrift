94f6db63907c0e450934d66f9ffb43985141684f
Knut-Frode Dagestad <knutfrode@gmail.com>
2020-12-15 14:13:20 +0100

    Fixed layout of example, and triggering regeneration

--- examples/example_huge_output.py
+++ examples/example_huge_output.py
@@ -9,7 +9,7 @@ import opendrift
 from opendrift.models.oceandrift import OceanDrift
 from opendrift.readers import reader_oscillating
 
-#%
+#%%
 # First make a simulation with two seedings, marked by *origin_marker*
 o = OceanDrift(loglevel=50)
 t1 = datetime.now()
@@ -30,14 +30,14 @@ o.set_config('drift:horizontal_diffusivity', 10)
 o.run(duration=timedelta(hours=24),
       time_step=900, time_step_output=1800, outfile=outfile)
 
-#%
+#%%
 # Opening the output file lazily with Xarray.
 # This will work even if the file is too large to fit in memory, as it
 # will read and process data chuck-by-chunk directly from file using Dask.
 # Note that the analysis file will be re-used if existing. Thus this file should be deleted after making any changes to the simulation above.
 o = opendrift.open_xarray(outfile, analysis_file='simulation_density.nc')
 
-#%
+#%%
 # Making two animations, for each of the two seedings / origin_markere.
 # The calculated density fields will be stored/cached in the analysis file
 # for later re-use, as their calculation may be time consuming
@@ -48,13 +48,13 @@ for om in [0, 1]:
     o.animation(density=True, density_pixelsize_m=500, fast=False,
                 origin_marker=om, show_elements=False, vmin=0, vmax=200)
 
-#%
+#%%
 # First seeding
 
 #%%
 # .. image:: /gallery/animations/example_huge_output_0.gif
 
-#%
+#%%
 # Second seeding
 
 #%%
