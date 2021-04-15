f802728b3f3ae54ab0761303dc9b1b48f0932605
Knut-Frode Dagestad <knutfrode@gmail.com>
2021-04-15 13:51:01 +0200

    Pre-retrieving html-pages of some thredds-datasets which are frequently unavailable, in order to wake up thredds-server.

--- examples/example_fvcom.py
+++ examples/example_fvcom.py
@@ -5,6 +5,7 @@ FVCOM: Using model input from unstructured grid
 """
 
 from datetime import timedelta
+import urllib.request as urllib_request
 import numpy as np
 from opendrift.readers import reader_netCDF_CF_unstructured
 from opendrift.readers import reader_global_landmask
@@ -13,6 +14,10 @@ from opendrift.models.oceandrift import OceanDrift
 o = OceanDrift(loglevel=20)  # Set loglevel to 0 for debug information
 
 # Reader
+for r in ['https://thredds.met.no/thredds/users.html', 'https://thredds.met.no/thredds/dodsC/metusers/knutfd/thredds/netcdf_unstructured_samples/AkvaplanNiva_sample_lonlat_fixed.nc.html']:
+    request = urllib_request.Request(r)  # Wake up thredds-server, if sleeping
+    urllib_request.urlopen(request, timeout=5)
+
 proj = "+proj=utm +zone=33W, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 fvcom = reader_netCDF_CF_unstructured.Reader(filename = 'https://thredds.met.no/thredds/dodsC/metusers/knutfd/thredds/netcdf_unstructured_samples/AkvaplanNiva_sample_lonlat_fixed.nc', proj4 = proj)
 o.add_reader(fvcom)
