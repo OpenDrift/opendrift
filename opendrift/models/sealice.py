199dc25bb39a98d1f1d88478f9a84e2f43d24293
julien <julien@geocluster.lan>
2021-06-16 17:46:39 +0100

    use of add_config to setup the parameters Use the biomass in elements and compute the population based on proportions of the reference population (linear relation)

--- opendrift/models/sealice.py
+++ opendrift/models/sealice.py
@@ -28,25 +28,33 @@ class SeaLiceElement(Lagrangian3DArray):
     """
 
     variables = Lagrangian3DArray.add_variables([
-
+        ('particle_biomass',{'dtype': np.float,
+                            'units': 'kg',
+                            'default': 1000.}),
+        ('hatched',{'dtype': np.float,
+                            'units': '',
+                            'default': 0.}),
         ('nauplii', {'dtype': np.float,
                             'units': '',
                             'default': 0.}),
         ('copepodid', {'dtype': np.float,
                      'units': '',
                      'default': 0.}),
+        ('dead', {'dtype': np.bool,
+                     'units': '',
+                     'default': False}),
         ('degree_days', {'dtype': np.float,
                      'units': '',
                     'default': 0}), #range 40-170
-        ('salinity_above', {'dtype': np.float,
-                     'units': 'PSU',
-                    'default': 35}),
+        ('salinity_above', {'dtype': np.bool,
+                     'units': '',
+                    'default': False}),
         ('temperature_above', {'dtype': np.float,
                      'units': '',
                     'default': 10}),
         ('temperature_below', {'dtype': np.float,
                      'units': '',
-                    'default': 10})
+                    'default': 10}),
         ('light', {'dtype': np.float,
                      'units': 'µmol photon  s−1 m−2',
                      'default': 0.})
@@ -80,72 +88,118 @@ class SeaLice(OceanDrift):
 
     # required_profiles_z_range = [0, -50]  # The depth range (in m) which profiles should cover
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,*args, **kwargs):
 
         # Calling general constructor of parent class
-        super(LarvalFish, self).__init__(*args, **kwargs)
+        super(SeaLice, self).__init__(*args, **kwargs)
 
-        # IBM configuration options
-        # self._add_config({
-        #     'IBM:fraction_of_timestep_swimming':
-        #         {'type': 'float', 'default': 0.15,
-        #          'min': 0.0, 'max': 1.0, 'units': 'fraction',
-        #          'description': 'Fraction of timestep swimming',
-        #          'level': self.CONFIG_LEVEL_ADVANCED},
-        #     })
+        # Configuration options
+        self._add_config({
+            'general:LicePerFish':{'type':np.float, 'default':0.5, # this could become an element property
+                                'min': 0.0, 'max': None, 'units': '',
+                                'description': 'Number of lice per fish',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:AvFishW8':{'type':np.float, 'default':4.5, # this could become an element property
+                                'min': 0.0, 'max': 1.0, 'units': 'fraction',
+                                'description': 'Average fish weight',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:seeding_time_step':{'type':np.float, 'default':None,
+                                'min': None, 'max': None, 'units': 'seconds',
+                                'description': 'Time between particle release',
+                                'level': self.CONFIG_LEVEL_ESSENTIAL},
+            'general:death_rate':{'type':np.float, 'default':0.01/3600,
+                                'min': 0., 'max': None, 'units': 's-1',
+                                'description': 'Rate of Larvae death per seconds',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:maturation_rate':{'type':np.float, 'default':0.1/3600,
+                                'min': 0., 'max': None, 'units': 's-1',
+                                'description': 'Rate of Nauplii maturation in Copepodids',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:maturity_date':{'type':np.float, 'default':3.63,
+                                'min': 0., 'max': None, 'units': 'days',
+                                'description': 'Days to start maturing into Copepodids',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:sinking_velocity':{'type':np.float, 'default':0.00025,
+                                'min': 0., 'max': 0.01, 'units': 'm.s-1',
+                                'description': 'Larvae sinking velocity',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:vertical_migration_speed':{'type':np.float, 'default':0.00075,
+                                'min': 0., 'max': 0.01, 'units': 'm.s-1',
+                                'description': 'Larvae vertical speed',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:freezing_salinity':{'type':np.float, 'default':27,
+                                'min': 0., 'max': 35., 'units': 'PSU',
+                                'description': 'Salinity immobilising larvae',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:avoided_salinity':{'type':np.float, 'default':32,
+                                'min': 0., 'max': 50., 'units': 'PSU',
+                                'description': 'Salinity actively avoided',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:nu':{'type':np.float, 'default':500.,
+                                'min': 100, 'max': 1000, 'units': 'nm',
+                                'description': 'Wavelength used to calculate irradiance',
+                                'level': self.CONFIG_LEVEL_ADVANCED},
+            'general:k_water':{'type':np.float, 'default':-0.2,
+                                'min': -10., 'max': 0., 'units': '',
+                                'description': 'coefficient of exponential decay of light in water',
+                                'level': self.CONFIG_LEVEL_ADVANCED},
+            'general:Nauplii_light_trigger':{'type':np.float, 'default':2.E-5,
+                                'min': 0., 'max': 1., 'units': 'µmol photon  s−1 m−2',
+                                'description': 'light detection threshold of Nauplii',
+                                'level': self.CONFIG_LEVEL_BASIC},
+            'general:Copepodid_light_trigger':{'type':np.float, 'default':0.392,
+                                'min': 0.0, 'max': 1.0, 'units': 'µmol photon  s−1 m−2',
+                                'description': 'light detection threshold of copepodids',
+                                'level': self.CONFIG_LEVEL_BASIC},
+             'general:twilight':{'type':np.float, 'default':15,
+                                'min': 0., 'max': 90., 'units': 'degrees',
+                                'description': 'angle below the horizon for twilight',
+                                'level': self.CONFIG_LEVEL_ADVANCED},
+            })
 
-        self._set_config_default('drift:vertical_mixing', True)
-        self.LicePerFish=0.5 # need to be added in module parameters
-        self.AvFishW8 = 4.5 # need to be added in module parameters
-        self.licing_time_step=timedelta(hours=1).total_seconds() # need to be added in module parameters kee it in seconds
-        self.ffarm="/home/julien/DATA/Marine plastics/PlasticsATBay/Simulations/oceano/opendrift/opendrift/scripts/sea_lice/farmdata.csv"
-        self.particle_prod= 50 #number of particle per farm per emission
-        self.Kp = 0.01/3600 #kill per second
-        self.Mp = 0.1/3600 #maturity per second
-        # change that into degree days in next generation
-        self.Mat = int(np.ceil(3.63*24*3600/self.time_step.total_seconds())) # maturity age in timestep
-        self.sinking_velocity = 0.00025 # m.s-1
-        self.vertical_migration_speed=0.00075 #m.s-1
+        # self._set_config_default('drift:vertical_mixing', True)
+
+        # self.LicePerFish=parameters['LicePerFish'] # need to be added in module parameters
+        # self.AvFishW8 = parameters['AvFishW8'] # need to be added in module parameters
+        # # self.licing_time_step= parameters['licing_time_step']# need to be added in module parameters kee it in seconds
+        # self.particle_biomass= parameters['particle_biomass'] #number of particle per farm per emission per hour
+        # self.death_rate = parameters['Killing_rate'] #kill per second
+        # self.Mp = parameters['Maturation_rate'] #maturity per second
+        # # change that into degree days in next generation
+        #
+        # self.sinking_velocity = parameters['sinking_velocity'] # m.s-1
+        # self.vertical_migration_speed=parameters['vertical_migration_speed'] #m.s-1
+        #
+        # self.freezing_salinity = parameters['freezing_salinity'] #PSU
+        # self.avoided_salinity=parameters['avoided_salinity'] # PSU
+        # self.nu=parameters['nu'] #mainwavelength of irradiance in nanometres
+        # self.k_water=parameters['k_water'] # parameter decay of light in m-1
+        # self.Nauplii_light_trigger= parameters['Nauplii_light_trigger']# µmol photon  s−1 m−2
+        # self.Copepodid_light_trigger=parameters['Copepodid_light_trigger'] # µmol photon  s−1 m−2
+        # self.twilight = np.deg2rad(parameters['twilight']) #Nautical horizon
+
+
+    def prepare_run(self):
         self.sensing_distance=2*self.vertical_migration_speed*self.time_step.total_seconds()
-        self.freezing_salinity = 27 #PSU
-        self.avoided_salinity=32 # PSU
-        self.nu=500 #mainwavelength of irradiance in nanometres
-        self.k_water=-0.2 # parameter decay of light in m-1
-        self.Nauplii_light_trigger= 2.E-5# µmol photon  s−1 m−2
-        self.Copepodid_light_trigger=0.392 # µmol photon  s−1 m−2
-        self.horizon = np.deg2rad(15) #Nautical horizon
-        self.tau= np.pi+2*self.horizon #width of solar irridation distribution
+        self.Mat = int(np.ceil(self.maturity_date*24*3600/self.time_step.total_seconds())) # maturity age in timestep
+        self.tau= np.pi+2*self.twilight #width of solar irridation distribution
         self.central_lon, self.central_lat=self.xy2lonlat((self.xmax-self.xmin)/2,
-                                        (self.ymax-self.ymin)/2)
+        (self.ymax-self.ymin)/2)
         logger.info("center of area for solar angle is lat: {}, lon: {}"\
-                    .format(self.central_lat,self.central_lon))
-        logger.info("Reading fish farm data...")
-        self.farms()
+        .format(self.central_lat,self.central_lon))
         logger.info("Making super individual population evolution...")
-        self.super_indiv_aging()
+        t=np.arange(0,(self.end_time-self.start_time).total_seconds(),\
+            self.time_step.total_seconds())
+        self.juv, self.adult, self.dead=population(t, self.Mat, self.maturation_rate, self.death_rate)
         logger.info("Loading NASA irradiance data...")
         from opendrift.readers.reader_netCDF_CF_generic import Reader
         r = Reader('https://opendap.larc.nasa.gov/opendap/hyrax/POWER/daily/\
-            power_801_daily_allsky_sfc_sw_dwn_lst.nc', standard_name_mapping=\
-            {'ALLSKY_SFC_SW_DWN': 'surface_net_downward_radiative_flux'})
+        power_801_daily_allsky_sfc_sw_dwn_lst.nc', standard_name_mapping=\
+        {'ALLSKY_SFC_SW_DWN': 'surface_net_downward_radiative_flux'})
+        self.add_reader(r)
         logger.info("NASA irradiance data reader ready.")
 
-    def farms(self):
-        """
-        Setup the parameters of the different fish farms
-        Returns
-        -------
-        Farms: np.array
-            All the farm parameters
-        PartMass: np.array
-            Biomass of fish in a particle
-        """
-        # headers name, region,pBiomass, X, Y, FMAbiomass, AvFishW8
-        DType=[ 'U20', 'U20', np.float, np.float, np.float, np.float, np.float]
-        self.farms=np.genfromtxt(self.ffarm, dtype=DType, delimiter=",",names=True, autostrip=True)
-        self.particle_biomass = self.farms["FMAbiomass"]/self.particle_prod
-
-    def new_born(self, A):
+    def new_born(self, Particle_biomass):
         """
         Approach by Rittenhouse et al., (2016) [Rittenhouse, M.A., C.W. Revie
         and A. Hurford, 2016. A model for sea lice (Lepeophtheirus salmonis)
@@ -162,10 +216,11 @@ class SeaLice(OceanDrift):
         and A(t) is the total number of adult female lice on each farm,
         derived here from lice target levels and number of fish on site.
         """
-        Ne, Eps, nu = 592, 0.0476, 0.6
-        return Ne* Eps* nu*A*self.LicePerFish/self.AvFishW8
+        Ne, nu = 592 , 0.6
+        Eps=0.0476/timedelta(days=1).total_seconds()*self.seeding_time_step
+        return Ne* Eps* nu*Particle_biomass*self.LicePerFish/self.AvFishW8
 
-    def population(t, Mat, Mp, Kp, Lice_parts):
+    def population(t, Mat, Mp, Kp):
         """
         Simulate the population evolution in each farm according to its Biomass
         Parameters
@@ -188,38 +243,48 @@ class SeaLice(OceanDrift):
         Juveniles(Nauplii), adults(Copepodid), dead lices arrays through
         the duration of the experiment
         """
-        juv=Lice_parts*np.exp(-Kp*t)
+        juv=np.exp(-Kp*t)
         dead=Lice_parts-juv
-        adults=np.zeros_like(t)
+        adult=np.zeros_like(t)
         decayjuv=juv[Mat-1]*np.exp(-(Mp+Kp)*(t[t>=Mat]-Mat))
         juv[t>=Mat] =decayjuv
-        adults[t>=Mat]=Lice_parts-dead[t>=Mat]-juv[t>=Mat]
-        return juv, adults, dead
+        adult[t>=Mat]-=dead[t>=Mat]-juv[t>=Mat]
+        return juv, adult, dead
 
-    def super_indiv_aging(self):
-        """
-        Create a dictionary of population evolution to read through the model
-        """
-        t = np.arange(0,(self.end_time-self.start_time).total_seconds(),\
-            self.time_step.total_seconds())
-        self.Juvs, self.Adults, self.Deads = np.zeros((len(self.particle_biomass), len(t))),
-            np.zeros((len(self.particle_biomass), len(t))), np.zeros((len(self.particle_biomass), len(t)))
-        self.desactivation_time=np.zeros(len(self.particle_biomass))
-        for k in range(len(self.particle_biomass)):
-            N0 = new_born(self, self.particle_biomass[k])
-            self.Juvs[k], self.Adults[k], self.Deads[k] = population(t, self.Mat,
-                                                        self.Mp, self.Kp,N0)
-            self.desactivation_time[k]= t[np.where(Deads[k]>N0-1)[0][0]] # all deads
+    # def super_indiv_aging(self):
+    #     """
+    #     Create a dictionary of population evolution to read through the model
+    #     """
+    #     t = np.arange(0,(self.end_time-self.start_time).total_seconds(),\
+    #         self.time_step.total_seconds())
+    #     self.Juvs, self.Adults, self.Deads = np.zeros((len(self.particle_biomass), \
+    #         len(t))), np.zeros((len(self.particle_biomass), len(t))), \
+    #         np.zeros((len(self.particle_biomass), len(t)))
+    #     self.desactivation_time=np.zeros(len(self.particle_biomass))
+    #     for k in range(len(self.particle_biomass)):
+    #         N0 = new_born(self, self.particle_biomass[k])
+    #         self.Juvs[k], self.Adults[k], self.Deads[k] = population(t, self.Mat,
+    #                                                     self.Mp, self.death_rate,N0)
+    #         self.desactivation_time[k]= t[np.where(Deads[k]>N0-1)[0][0]] # all deads
 
     def SI_pop(self):
         """
         distribute the age fractions in the particles
         """
-        #New_release = np.where(self.elements.age_seconds<self.time_step)[0]
-        self.deactivate_elements(self.elements.age_seconds>self.desactivation_time[ \
-                                self.elements.origin_marker], reason="All dead")
-        self.elements.nauplii=self.Juvs[self.element.origin_marker, self.elements.age_seconds]
-        self.elements.copepodid=self.Juvs[self.element.origin_marker, self.elements.age_seconds]
+        # First desactivate dead particles
+        dead=self.elements.dead
+        alive= np.logical_not(dead)
+        Dying=self.elements.hatched[alive]<self.elements.nauplii[alive]+self.elements.copepodid[alive]+1
+        self.elements.dead[Dying]=True
+        self.deactivate_elements(self.elements.dead, reason="All dead")
+        # Take care of new borns
+        alive= np.logical_not(dead)
+        New_release = self.elements.age_seconds[alive]<self.time_step
+        self.elements.hatched[New_release]=new_born(self, self.elements.Particle_biomass[New_release])
+        # Make others mature
+        Free = np.logical_not(New_release & dead)
+        self.elements.nauplii[Free]=self.elements.hatched[Free]*self.juv[self.elements.age_seconds[Free]]
+        self.elements.copepodid[Free]=self.elements.hatched[Free]*self.adult[self.elements.age_seconds[Free]]
 
     def sensing(self, Normal_salt):
         """
@@ -235,7 +300,7 @@ class SeaLice(OceanDrift):
         self.elements.temperature_below[Normal_salt]= self.environment.sea_water_temperature
         self.elements.z[Normal_salt] = Backup
 
-    def degree_days():
+    def degree_days(self):
         """
         Calculate the degree days of a particles
         >>> under development <<<
@@ -246,7 +311,7 @@ class SeaLice(OceanDrift):
     def solar_energy_distribution(self):
         """
         Distribute the daily energy from irradiance with a gaussian distribution.
-        We use the nautical horizon (-15 degrees)
+        We use the twilight
         """
         self.solar_angle=np.deg2rad(self.solar_elevation(self.time,
                                     self.central_lon, self.central_lat))
@@ -262,18 +327,21 @@ class SeaLice(OceanDrift):
         return Irr0*np.exp(self.k_water*z)
 
     def Lice_vertical_migration(self):
+        """
+        Make larval sea lice to migrate vertically according to salinity, light
+        and temperature triggers.
+        """
         ### all lice sink at the same speed
         self.elements.z -= self.sinking_velocity*self.time_step.total_seconds()
 
         ### filter actively avoiding salt
         Frozen=self.environment.sea_water_salinity<self.freezing_salinity
-        Avoiding = (self.freezing_salinity< self.environment.sea_water_salinity)\
-                || (self.environment.sea_water_salinity<self.avoided_salinity)
+        Avoiding = np.logical_and((self.freezing_salinity< self.environment.sea_water_salinity),
+                (self.environment.sea_water_salinity<self.avoided_salinity))
         Normal_salt = self.environment.sea_water_salinity>=self.avoided_salinity
 
         self.sensing(self, Normal_salt)
         ### Irradiance computation
-
         self.elements.light = irradiance(self, self.environment.surface_net_downward_radiative_flux,
                             self.elements.z)
         ### identify the elements involved in the different scenarios
