6d28504448c11e76dc11fdb5cb30ccc76d95f697
knutfrode <knutfrode@gmail.com>
2021-07-09 14:58:00 +0200

    Fixed bug in example_map

--- examples/example_map.py
+++ examples/example_map.py
@@ -21,9 +21,10 @@ sp = ccrs.Stereographic(central_longitude=0, central_latitude=60)
 ax = fig.add_subplot(1, 1, 1, projection=sp)
 
 corners = [-30, 30, 45, 75]
-s = cfeature.AdaptiveScaler('coarse',
-                (('low', 100), ('intermediate', 20), ('high', 5), ('full', 1)))
-lscale = s.scale_from_extent([corners[0], corners[1], corners[2], corners[3]])
+#s = cfeature.AdaptiveScaler('coarse',
+#                (('low', 100), ('intermediate', 20), ('high', 5), ('full', 1)))
+#lscale = s.scale_from_extent([corners[0], corners[1], corners[2], corners[3]])
+lscale = 'auto'
 print(lscale, 'LSCALE')
 f = cfeature.GSHHSFeature(scale=lscale, levels=[1])
 ax.add_geometries(
