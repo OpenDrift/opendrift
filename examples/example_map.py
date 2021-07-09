df9952a909f97c35947128430dd49932b602e955
knutfrode <knutfrode@gmail.com>
2021-07-09 17:47:26 +0200

    Example_map now plots with all coastline resolutions

--- examples/example_map.py
+++ examples/example_map.py
@@ -7,34 +7,28 @@ Plotting map
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import cartopy
-import shapely
-#from opendrift.models.oceandrift import OceanDrift
-#o = OceanDrift(loglevel=0)
-#from opendrift_landmask_data import Landmask
 
-print(cartopy.__version__)
-print(shapely.__version__)
 
-fig = plt.figure()
-sp = ccrs.Stereographic(central_longitude=0, central_latitude=60)
-ax = fig.add_subplot(1, 1, 1, projection=sp)
+for lscale in ['coarse', 'low', 'intermediate', 'high', 'auto']:
+    fig = plt.figure()
+    sp = ccrs.Stereographic(central_longitude=0, central_latitude=60)
+    ax = fig.add_subplot(1, 1, 1, projection=sp)
 
-corners = [-30, 30, 45, 75]
-#s = cfeature.AdaptiveScaler('coarse',
-#                (('low', 100), ('intermediate', 20), ('high', 5), ('full', 1)))
-#lscale = s.scale_from_extent([corners[0], corners[1], corners[2], corners[3]])
-lscale = 'auto'
-print(lscale, 'LSCALE')
-f = cfeature.GSHHSFeature(scale=lscale, levels=[1])
-ax.add_geometries(
-    #f.intersecting_geometries(corners),
-    f.geometries(),
-    ccrs.PlateCarree(),
-    facecolor=cfeature.COLORS['land'],
-    edgecolor='black')
+    corners = [-30, 30, 45, 75]
+    #s = cfeature.AdaptiveScaler('coarse',
+    #                (('low', 100), ('intermediate', 20), ('high', 5), ('full', 1)))
+    #lscale = s.scale_from_extent([corners[0], corners[1], corners[2], corners[3]])
+    #lscale = 'auto'
+    print(lscale, 'LSCALE')
+    f = cfeature.GSHHSFeature(scale=lscale, levels=[1])
+    ax.add_geometries(
+        #f.intersecting_geometries(corners),
+        f.geometries(),
+        ccrs.PlateCarree(),
+        facecolor=cfeature.COLORS['land'],
+        edgecolor='black')
 
-ax.set_extent(corners, crs=ccrs.PlateCarree())
-gl = ax.gridlines(ccrs.PlateCarree())
+    ax.set_extent(corners, crs=ccrs.PlateCarree())
+    gl = ax.gridlines(ccrs.PlateCarree())
 
-plt.show()
+    plt.show()
