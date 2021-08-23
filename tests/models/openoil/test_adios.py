d8f4e8e9445c3b02fb483538dd04c08f2bf8784c
Gaute Hope <gaute.hope@met.no>
2021-08-23 14:30:54 +0200

    adios: check for unique oils

--- tests/models/openoil/test_adios.py
+++ tests/models/openoil/test_adios.py
@@ -1,5 +1,10 @@
 from opendrift.models.openoil import adios
 
 def test_get_all():
-    oils = adios.oils(1)
-    print(oils)
+    oils = adios.oils(300)
+    assert len(oils) == 300
+
+    # make sure they are all unique
+    ids = [o._id for o in oils]
+    sids = set(ids)
+    assert len(ids) == len(sids)
