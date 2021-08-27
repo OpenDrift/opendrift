a2ece96b1ae5f930e50fcc580da4293cb65ede4c
Gaute Hope <gaute.hope@met.no>
2021-08-27 10:41:42 +0200

    test: add --show-plot option and test fast + slow plots

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,11 +8,14 @@ def pytest_addoption(parser):
     parser.addoption(
             "--run-very-slow", action="store_true", default=False, help="run very slow tests"
             )
+    parser.addoption(
+            "--show-plot", action="store_true", default=False, help="show plots"
+            )
 
 
-    def pytest_configure(config):
-        config.addinivalue_line("markers", "slow: mark test as slow to run")
-        config.addinivalue_line("markers", "veryslow: mark test as very slow to run")
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "veryslow: mark test as very slow to run")
 
 
 def pytest_collection_modifyitems(config, items):
@@ -28,3 +31,4 @@ def pytest_collection_modifyitems(config, items):
             if "veryslow" in item.keywords:
                 item.add_marker(skip_veryslow)
 
+from .fixtures import *
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -2,9 +2,13 @@ import pytest
 import os
 
 @pytest.fixture
-def test_data ():
+def test_data():
     import opendrift
     return os.path.abspath(
             os.path.join(os.path.dirname(opendrift.__file__),
                         '..', 'tests', 'test_data')) + os.path.sep
+
+@pytest.fixture
+def show_plot(pytestconfig):
+    return pytestconfig.getoption('show_plot')
 
