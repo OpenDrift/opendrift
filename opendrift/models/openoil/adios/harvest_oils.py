1d771cb7fbedac1fa366c196ec3e421fcebe6384
Gaute Hope <gaute.hope@met.no>
2021-09-02 13:44:58 +0200

    harvet oils: make independet of api

--- opendrift/models/openoil/adios/harvest_oils.py
+++ opendrift/models/openoil/adios/harvest_oils.py
@@ -1,26 +1,77 @@
-from opendrift.models.openoil import adios
-from opendrift.models.openoil.adios.oil import OpendriftOil
 from pathlib import Path
-import shutil
 import sys
-
 import requests
-requests.packages.urllib3.disable_warnings()
+from opendrift.models.openoil.adios.oil import OpendriftOil
 
 import coloredlogs
 import logging
 logger = logging.getLogger('harvest_oils')
-coloredlogs.install('debug')
 
-dst = Path('oils')
+ADIOS = "https://adios.orr.noaa.gov/api/oils/"
 
-if not dst.exists():
-    raise Exception(f"destination path does not exist: {dst}")
+def get_full_oil_from_id(_id):
+    logger.debug(f"Fetching full oil: {_id}")
+    o = requests.get(f"{ADIOS}/{_id}")
+    o.raise_for_status()
+    return OpendriftOil(o.json())
 
-if not '--skip-download' in sys.argv:
-    for o in adios.oils(None):
-        logger.info(f"fetching oil: {o.id} / {o.name}")
-        o = o.make_full()
+def oils(limit=50, query=''):
+    """
+    Get all oils.
+
+    Args:
+
+        limit: number of oils to retrieve, None means all available.
+        query: search string (name, id, location).
+
+
+    Returns:
+
+        List of `class:ThinOil`s.
+    """
+    # The batch size seems to be maximum 205 at the moment.
+    MAX_BATCH_SZ = 205
+
+    batch = min(MAX_BATCH_SZ, limit if limit is not None else MAX_BATCH_SZ)
+
+    oils = []
+
+    # XXX: This may fail when batch size is less or unequal to `batch`.
+    while limit is None or len(oils) < limit:
+        p = int(len(oils) / batch)  # next page, XXX: check for off-by-one?
+        logging.debug(
+            f"Requesting list of oils from ADIOS, oils: {len(oils)}, page: {p}"
+        )
+
+        o = requests.get(ADIOS, {
+            'dir': 'asc',
+            'limit': batch,
+            'page': p,
+            'sort': 'metadata.name',
+            'q': query
+        })
+        o.raise_for_status()
+        o = o.json()
+
+        oils.extend(o['data'])
+
+        if len(oils) >= int(o['meta']['total']) or (limit is not None and len(oils) >= limit):
+            break
+
+    logger.info(f"Fetched {len(oils)} oils from ADIOS")
+
+    limit = len(oils) if limit is None else limit
+    oils = oils[:min(limit, len(oils))]
+    oils = [o['_id'] for o in oils]
+
+    return oils
+
+def download(dst):
+    logger.info('downloading all oils..')
+
+    for o in oils(None):
+        logger.info(f"fetching oil: {o}..")
+        o = get_full_oil_from_id(o)
         if o.valid():
             f = dst / Path(o.id).with_suffix('.json')
             with open(f, 'w') as fd:
@@ -28,38 +79,35 @@ if not '--skip-download' in sys.argv:
         else:
             logger.warning(f'skipping invalid oil: {o.id}')
 
-logger.info('making archive')
-## Tarfile
-# import glob
-# import tarfile
-# with tarfile.open('oils.tar.xz', 'x:xz') as c:
-#     for f in glob.glob('oils/*.json'):
-#         logger.debug(f'adding {f} to archive')
-#         c.add(f, Path(f).name)
+def make_archive(oildir, file):
+    logger.info(f'making archive: {oildir / "*.json"}')
 
-## Big json
-oils = []
+    # Making a big JSON array with oils as a dictionary each.
+    oils = []
 
-import glob
-import json
-import lzma
-for f in glob.glob('oils/*.json'):
-    with open(f, 'r') as fd:
-        oils.append(json.load(fd))
+    import glob
+    import json
+    import lzma
+    for f in glob.glob(str(oildir / '*.json')):
+        with open(f, 'r') as fd:
+            oils.append(json.load(fd))
 
-logger.info(f'added {len(oils)} oils..')
-with lzma.open('oils.xz', 'xt') as c:
-    json.dump(oils, c)
+    logger.info(f'added {len(oils)} oils..')
+    logger.info(f'compressing to {file}..')
+    with lzma.open(file, 'wt') as c:
+        json.dump(oils, c)
 
-## pickle
-# import glob
-# import pickle
-# import json
-# oils = []
-# for f in glob.glob('oils/*.json'):
-#     with open(f, 'r') as fd:
-#         oils.append(OpendriftOil(json.load(fd)))
 
-# logger.info(f'added {len(oils)} oils..')
-# with open('oils.db', 'wb') as fd:
-#     pickle.dump(oils, fd)
+if __name__ == '__main__':
+    coloredlogs.install('debug')
+
+    dst = Path('oils')
+
+    if not dst.exists():
+        raise Exception(f"destination path does not exist: {dst}")
+
+    if not '--skip-download' in sys.argv:
+        download(dst)
+
+    make_archive(dst, 'oils.xz')
+
