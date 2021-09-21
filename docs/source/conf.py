5df49f500896cff732b1f0e17814e8b607869c32
Gaute Hope <gaute.hope@met.no>
2021-09-21 13:00:00 +0200

    docs: fix css override

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,10 +85,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
-
+html_css_files = [
+        'theme_overrides.css'
+        ]
 
