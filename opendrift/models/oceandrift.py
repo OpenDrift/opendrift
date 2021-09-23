Fixed sign error of depth below MLD in Sundby parameterisation of vertical diffusivity. Made new config setting verticalmixing:background_diffusivity to be used below MLD for Sundby and Large parameterisations

opendrift/models/oceandrift.py
@@ -105,6 +105,9 @@ class OceanDrift(OpenDriftSimulation):
                 'enum': ['environment', 'stepfunction', 'windspeed_Sundby1983',
                  'windspeed_Large1994', 'gls_tke','constant'], 'level': self.CONFIG_LEVEL_ADVANCED,
                  'units': 'seconds', 'description': 'Algorithm/source used for profile of vertical diffusivity. Environment means that diffusivity is aquired from readers or environment constants/fallback.'},
+            'vertical_mixing:background_diffusivity': {'type': 'float', 'min': 0, 'max': 1, 'default': .001,
+                'level': self.CONFIG_LEVEL_ADVANCED, 'units': 'm2s-1', 'description':
+                'Background diffusivity used below mixed layer for wind parameterisations.'},
             'vertical_mixing:TSprofiles': {'type': 'bool', 'default': False, 'level':
                 self.CONFIG_LEVEL_ADVANCED,
                 'description': 'Update T and S profiles within inner loop of vertical mixing. This takes more time, but may be slightly more accurate.'},
@@ -234,13 +237,15 @@ class OceanDrift(OpenDriftSimulation):
         pass
 
     def get_diffusivity_profile(self, model):
-        depths = self.environment_profiles['z']
+        depths = np.abs(self.environment_profiles['z'])
         wind, depth = np.meshgrid(self.wind_speed(), depths)
+        MLD = 50  # TODO: obtain from readers/environment
+        background_diffusivity = self.get_config('vertical_mixing:background_diffusivity')
 
         if model == 'windspeed_Large1994':
-            return verticaldiffusivity_Large1994(wind, depth)
+            return verticaldiffusivity_Large1994(wind, depth, MLD, background_diffusivity)
         elif model == 'windspeed_Sundby1983':
-            return verticaldiffusivity_Sundby1983(wind, depth)
+            return verticaldiffusivity_Sundby1983(wind, depth, MLD, background_diffusivity)
         elif model == 'gls_tke':
             if not hasattr(self, 'gls_parameters'):
                 logger.info('Searching readers for GLS parameters...')