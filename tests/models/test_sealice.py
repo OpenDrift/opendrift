9f32ef42c991c460c94242ef9b777987cf0f921d
Gaute Hope <gaute.hope@met.no>
2021-10-18 12:22:27 +0200

    use light from larc + re-enable land mask

--- tests/models/test_sealice.py
+++ tests/models/test_sealice.py
@@ -4,23 +4,6 @@ from opendrift.models.sealice import SeaLice
 from datetime import timedelta
 
 
-def test_sealice():
-    o = SeaLice(loglevel=30)
-    reader_arome = reader_netCDF_CF_generic.Reader(
-        o.test_data_folder() +
-        '2Feb2016_Nordic_sigma_3d/AROME_MetCoOp_00_DEF_20160202_subset.nc')
-    o.add_reader([reader_arome])
-    lat = 67.711251
-    lon = 13.556971  # Lofoten
-    o.seed_elements(lon,
-                    lat,
-                    radius=5000,
-                    number=1000,
-                    time=reader_arome.start_time)
-    o.run(steps=24, time_step=3600)
-    np.testing.assert_almost_equal(o.elements.lon.max(), 15.864, 2)
-
-
 def test_sealice_larc():
     o = SeaLice(loglevel=30)
     reader_arome = reader_netCDF_CF_generic.Reader(
@@ -28,7 +11,7 @@ def test_sealice_larc():
         '2Feb2016_Nordic_sigma_3d/AROME_MetCoOp_00_DEF_20160202_subset.nc')
     o.add_reader([reader_arome])
     reader_light = reader_netCDF_CF_generic.Reader(
-        'https://opendap.larc.nasa.gov/opendap/hyrax/POWER/daily/power_801_daily_allsky_sfc_sw_dwn_lst.nc',
+        'https://opendap.larc.nasa.gov/opendap/hyrax/POWER/monthly/power_901_monthly_radiation_utc.nc',
         standard_name_mapping={
             'ALLSKY_SFC_SW_DWN': 'surface_net_downward_radiative_flux'
         })
@@ -38,7 +21,6 @@ def test_sealice_larc():
     lat = 67.711251
     lon = 13.556971  # Lofoten
     o.set_config('lice:seeding_time_step', lcts)
-    o.set_config('general:use_auto_landmask', False)
     o.set_config('general:duration', length.total_seconds())
     o.seed_elements(lon,
                     lat,
