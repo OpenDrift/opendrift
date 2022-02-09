The factor 2 in previous commit turned out to not be a bug after all

opendrift/models/basemodel.py
@@ -2383,9 +2383,9 @@ class OpenDriftSimulation(PhysicsMethods, Timeable):
             logger.debug('Horizontal diffusivity is 0, no random walk.')
             return
         dt = np.abs(self.time_step.total_seconds())
-        x_vel = self.elements.moving * np.sqrt(D/dt) * np.random.normal(
+        x_vel = self.elements.moving * np.sqrt(2*D/dt) * np.random.normal(
             scale=1, size=self.num_elements_active())
-        y_vel = self.elements.moving * np.sqrt(D/dt) * np.random.normal(
+        y_vel = self.elements.moving * np.sqrt(2*D/dt) * np.random.normal(
             scale=1, size=self.num_elements_active())
         speed = np.sqrt(x_vel * x_vel + y_vel * y_vel)
         logger.debug(