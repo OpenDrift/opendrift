ab4cfd005e61763f934cc81c9f0bc7c662981f36
Knut-Frode Dagestad <knutfrode@gmail.com>
2022-03-05 12:00:51 +0100

    ROMS native reader now detects variables having standard_name attribute

--- examples/example_long_cmems_json.py
+++ examples/example_long_cmems_json.py
@@ -13,7 +13,7 @@ o = Leeway()
 # Adding a CMEMS reader as a JSON string, and NCEP winds from thredds URL.
 # For CMEMS, username or password must be stored in a .netrc file under
 # machine name "cmems", or provided explicitly instead of *null*
-o.add_readers_from_list(['{"reader": "reader_cmems", "dataset": "global-analysis-forecast-phy-001-024-hourly-t-u-v-ssh", "cmems_user": null, "cmems_password": null}',
+o.add_readers_from_list(['https://nrt.cmems-du.eu/thredds/dodsC/cmems_mod_glo_phy_anfc_merged-uv_PT1H-i',
     'https://pae-paha.pacioos.hawaii.edu/thredds/dodsC/ncep_global/NCEP_Global_Atmospheric_Model_best.ncd'])
 
 o.seed_elements(time=datetime.utcnow(), lon=123, lat=-16.1,
