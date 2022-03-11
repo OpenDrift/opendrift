26b520d9a4dec19bc6da97ab31a065a7691ec93b
knutfrode <knutfrode@gmail.com>
2022-03-11 17:12:13 +0100

    Added 7 norwegian oils

--- opendrift/models/openoil/adios/extra_oils/templates/parse_oil_pdf.py
+++ opendrift/models/openoil/adios/extra_oils/templates/parse_oil_pdf.py
@@ -1,22 +1,27 @@
+import sys
 import os
 import numpy as np
 import pandas as pd
 import zipfile
 
+np.set_printoptions(suppress=True)
+
 template = {
     'rows': {
         'density': {
-            'names': ['Specific gravity'],
+            'names': ['Specific gravity', 'Density'],
             'factor': 1000  # Convert g/l to kg/m3
             },
         'weathered': {
-            'names': ['Residue (wt'],
-            'subtract_from': 100
+            'names': ['Residue (wt', 'Weight Residue', 'Vol, Topped'],
+            #'names': ['Weight Residue'],
+            'subtract_from': 1,
+            'factor': .01
             },
-        #'viscosity': {
-        #    'names': ['Viscosity at 13'],
-        #    'factor': 1
-        #    },
+        'viscosity': {
+            'names': ['Viscosity at 13°C (cP) to predict', 'Viscosity of water-free'],
+            'factor': 0.001
+            },
         'wax_content': {
             'names': ['Wax Content'],
             'factor': 0.01
@@ -26,15 +31,20 @@ template = {
             'factor': 0.01
             },
         'pour_point': {
-            'names': ['Pour Point'],
+            'names': ['Pour point', 'Pour Point'],
+            'add_to': 273,
+            },
+        'flash_point': {
+            'names': ['Flash point', 'Flash Point'],
+            'add_to': 273,
             },
         },
     'columns': {
-        'boiling_point_temp': {'names': ['Temp.(']},
+        'boiling_point_temp': {'names': ['Temp.']},
         }
     }
 
-f = 'EDOCS-#51667-v1-Njord_2003_Forvitringsrapport.PDF'
+f = sys.argv[1]
 b = f.split('.')[0]
 
 if not os.path.exists(b+'.zip'):
@@ -51,33 +61,102 @@ print('Parsing zipfile ' + b+'.zip')
 for table in z.namelist():
     #print('Parsing table ' + table)
     df = pd.read_csv(z.open(table))
+    #print(df)
     # Search for row parameters
     for v, r in template['rows'].items():
         for name in r['names']:
             for i, row in df.iterrows():
                 if name in str(row):
-                    results[v] = []
+                    varname = v
+                    while(varname) in results:
+                        varname = varname + 'a'
+                    results[varname] = []
+                    #print(varname + str(row.values))
+                    for ri in range(len(row.values)):
+                        try:
+                            row.values[ri] = np.float32(row.values[ri].replace(',', '.'))
+                        except:
+                            pass
                     try:
-                        for aval in np.array([rva.replace(',', '.') for rva in np.array(row.values)]):
+                        for aval in row.values:
                             try:
-                                results[v].append(np.float32(aval.replace(',', '.')))
+                                results[varname].append(np.float32(aval))
                             except:
                                 pass
-                        results[v] = np.array(results[v])
+                        results[varname] = np.array(results[varname])
                         if 'factor' in r:
-                            results[v] = results[v]*r['factor']
+                            results[varname] = results[varname]*r['factor']
                         if 'subtract_from' in r:
-                            results[v] = r['subtract_from'] - results[v]
+                            results[varname] = r['subtract_from'] - results[varname]
+                        if 'add_to' in r:
+                            results[varname] = r['add_to'] + results[varname]
+                        #print(results[varname])
                     except:
-                        #print('Could not parse row: ' + str(row))
+                        print('Could not parse row: ' + str(row))
+                        stop
                         pass
 
+    #print('='*30)
+    #print(df)
+    #print('='*30)
     for v, r in template['columns'].items():
-        for name in r['names']:
-            for i, row in df.iterrows():
-                if name in str(row):
-                    print(df)
-                    #jakka
+        headerline = df.columns
+        if 'Temp' in headerline[0] and ('Vol' in headerline[1] or 'Duva' in headerline[1]):
+            cut_temps = df.iloc[:,0].values + 273
+            cut_vols = df.iloc[:,1].values
+            try:
+                cut_vols = cut_vols / 100
+            except:
+                cut_vols = [np.float64(val.replace(',', '.'))/100 for val in cut_vols]
 
+            re = np.empty(len(cut_temps)*3)
+            for n in range(len(cut_temps)):
+                re[n*3] = cut_temps[n]
+                re[n*3+2] = cut_vols[n]
+            print('Cuts:\n' + np.array2string(re.astype(np.float32), separator='\t').replace('\n', '\t'))
+#print('='*30)
+#print(pd.read_csv(z.open(z.namelist()[0])))
+#print('='*30)
 for n, r in results.items():
-    print(n, r)
+    if len(r) > 0 and np.sum(np.isfinite(r))>0:
+        print(n, r)
+print('='*30)
+
+for aas in ['', 'a', 'aa', 'aaa', 'aaaa', 'aaaaa']:
+    dstring = 'density' + aas
+    vstring = 'viscosity' + aas
+    wstring = 'weathered' + aas
+    rd = np.empty(12)
+    rv = np.empty(12)
+    for i in range(4):
+        if dstring in results and len(results[dstring])>0:
+            rd[i*3] = results[dstring][i]
+            rd[i*3+1] = 286
+            rd[i*3+2] = results[wstring][i]
+        if vstring in results and len(results[vstring])>0:
+            rv[i*3] = results[vstring][i]
+            rv[i*3+1] = 286
+            rv[i*3+2] = results[wstring][i]
+
+    if vstring in results:
+        print(vstring, np.array2string(rv.astype(np.float32), separator='\t').replace('\n', '\t'))
+    if dstring in results:
+        print(dstring, np.array2string(rd.astype(np.float32), separator='\t').replace('\n', '\t'))
+
+    
+#try:
+#    r = np.empty(12)
+#    for i in range(4):
+#        r[i*3] = results['density'][i]
+#        r[i*3+1] = 286
+#        r[i*3+2] = results['weathered'][i]
+#    print(np.array2string(r.astype(np.float32), separator='\t').replace('\n', '\t'))
+#except:
+#    print('No density')
+#
+#try:
+#    for i in range(4):
+#        r[i*3] = results['viscosity'][i]
+#    print(np.array2string(r.astype(np.float32), separator='\t').replace('\n', '\t'))
+#except:
+#    print('No viscosity')
