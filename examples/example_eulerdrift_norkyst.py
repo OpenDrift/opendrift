728f3b4cdc41e1035c91c449a91bc889e81f13b9
Gaute Hope <gaute.hope@met.no>
2022-05-31 15:40:07 +0200

    examples: advent -> eulerdrift

--- examples/example_eulerdrift_norkyst.py
+++ examples/example_eulerdrift_norkyst.py
@@ -1,24 +1,25 @@
 """
-Use nordic ocean model from via opendrift
-=========================================
+Euler simulation / Finite difference of blob with the Norkyst nordic ocean model
+================================================================================
 """
-import advent
 import logging
 import opendrift
 import matplotlib.pyplot as plt
 from opendrift.readers import reader_netCDF_CF_generic
 from opendrift.models.basemodel import OpenDriftSimulation
+from opendrift.models import eulerdrift
+
 
 logging.getLogger('opendrift').setLevel(logging.DEBUG)
-advent.install_logs()
+eulerdrift.install_logs() #remove
 
 reader_norkyst = reader_netCDF_CF_generic.Reader('https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
 # reader_norkyst = reader_netCDF_CF_generic.Reader(OpenDriftSimulation.test_data_folder(None) +  '16Nov2015_NorKyst_z_surface/norkyst800_subset_16Nov2015.nc')
 lon0, lat0 = reader_norkyst.xy2lonlat(reader_norkyst.xmin, reader_norkyst.ymin)
 
-s = advent.ExplSimulation.new(5.21, 60.19, 10., shape = (400, 400))
+s = eulerdrift.ExplSimulation.new(5.21, 60.19, 10., shape = (400, 400))
 s.t0 = reader_norkyst.start_time
-s.readers.append(advent.OpendriftReader(reader_norkyst))
+s.readers.append(eulerdrift.OpendriftReader(reader_norkyst))
 
 print(s.U(0.0))
 
