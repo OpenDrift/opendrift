Updating numerical tests to pass after recent bugfix in OpenOil terminal velocities

tests/models/openoil/test_oillibrary.py
@@ -158,14 +158,14 @@ def test_dispersion():
                 meanlon = 4.81742
             elif oil == 'SMORBUKK KONDENSAT' and windspeed == 8:
                 fraction_dispersed = 0.092
-                fraction_submerged = 0.390
-                fraction_evaporated = 0.473
-                meanlon = 4.809
+                fraction_submerged = 0.288
+                fraction_evaporated = 0.517
+                meanlon = 4.816
             elif oil == 'SKRUGARD' and windspeed == 8:
                 fraction_dispersed = 0.139
-                fraction_submerged = 0.481
+                fraction_submerged = 0.418
                 fraction_evaporated = 0.123
-                meanlon = 4.819
+                meanlon = 4.825
             else:
                 fraction_dispersed = -1  # not defined
 
@@ -197,7 +197,7 @@ def test_no_dispersion():
     b = o.get_oil_budget()
     actual_dispersed = b['mass_dispersed'] / b['mass_total']
     np.testing.assert_almost_equal(actual_dispersed[-1], 0)
-    np.testing.assert_array_almost_equal(o.elements.lon[4:7], [4.808, 4.796, 4.8], 3)
+    np.testing.assert_array_almost_equal(o.elements.lon[4:7], [4.804, 4.796, 4.8], 3)
 
 
 def test_biodegradation():