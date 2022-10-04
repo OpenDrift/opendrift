e70102bcfbd66483c0d649158dc9c367afd22a0e
Manuel Aghito <manuel.aghito@met.no>
2022-10-04 14:40:56 +0200

    Speeding up allocation of self.history in import_file

--- opendrift/export/io_netcdf.py
+++ opendrift/export/io_netcdf.py
@@ -325,7 +325,8 @@ def import_file(self, filename, times=None, elements=None, load_history=True):
     if load_history is True:
         self.history = np.ma.array(
             np.zeros([num_elements, self.steps_output]),
-            dtype=history_dtype, mask=[True])
+            dtype=history_dtype)
+        self.history = =np.ma.masked
         for var in infile.variables:
             if var in ['time', 'trajectory']:
                 continue
