e688d0858e0894893144072b5267a8a046ee4da0
Knut-Frode Dagestad <knutfrode@gmail.com>
2022-10-27 16:04:40 +0200

    Further illustrations of the flexibility and coolness of the new approach

--- examples/trajectory_analysis/demo_trajectory_analysis.py
+++ examples/trajectory_analysis/demo_trajectory_analysis.py
@@ -28,8 +28,8 @@ fig.savefig('testplot.png')
 # Extracting only the first 100 elements, and every 4th output time steps:
 dsub = d.isel(trajectory=range(0, 100), time=range(0, len(d.time), 4))
 ta.plot(dsub)
-
-# Plotting a "mean" trajectory
+#
+## Plotting a "mean" trajectory
 dmean = d.mean('trajectory', skipna=True)
 ta.plot(dmean, trajectory_kwargs={'color': 'red', 'linewidth': 5})
 
@@ -37,4 +37,7 @@ ta.plot(dmean, trajectory_kwargs={'color': 'red', 'linewidth': 5})
 ax, fig, gcrs = ta.set_up_map(d, land_color='green')
 ax.plot(d.lon.T, d.lat.T, color='red', alpha=0.01, transform=gcrs)  # Plotting trajectories in red
 ax.plot(dmean.lon.T, dmean.lat.T, color='black', alpha=1, linewidth=5, transform=gcrs)  # Plotting mean trajectory in black
+# Plotting the mean trajectory for a sub period in yellow
+dmean17nov = d.sel(time=slice('2015-11-17', '2015-11-17 12')).mean('trajectory', skipna=True)
+ax.plot(dmean17nov.lon.T, dmean17nov.lat.T, color='yellow', alpha=1, linewidth=5, transform=gcrs)
 plt.show()
