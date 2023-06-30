b1aff0da6167c47aca4d53dc771edc10b5d7e9ee
Gaute Hope <gaute.hope@met.no>
2023-06-30 11:43:47 +0200

    ... empty ...

--- opendrift/physics/waveperiod.py
+++ opendrift/physics/waveperiod.py
@@ -1,44 +1,108 @@
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from enum import Enum
+from typing import Optional
+import numpy as np
+
 
 class Spectrum(Enum):
     JONSWAP = 1
+    Phillips = 2
+
+
 
 class T(ABC):
     """
     Generic Wave Period.
     """
+    _t_: float
+    js: 'SpecCoeff'
+    phillips: 'SpecCoeff'
 
-    @abstractmethod
-    def tp(self, spectrum: Spectrum) -> float:
+    @property
+    def t(self):
+        return self._t_
+
+    @property
+    def T(self):
+        return self._t_
+
+    def __init__(self, t):
+        self._t_ = t
+
+    def tp(self, spectrum: Spectrum) -> 'T':
         """
         Return a interpretation of the wave period in the given spectrum.
         """
-        raise NotImplemented()
+        return self.into(Tp, spectrum)
 
+    def into(self, ty: 'T', spectrum) -> 'T':
+        if isinstance(self, ty):
+            return self
+
+        match spectrum:
+            case Spectrum.JONSWAP:
+                return ty(self.js.coeff(ty) * self._t_)
+
+
+
+@dataclass
+class SpecCoeff:
+    tp: Optional[float]
+    tm_01: float
+    tm_10: float
+    tm_02: float
+    tm_03: float
+
+    def coeff(self, ty) -> float:
+        if ty == Tp:
+            return self.tp
+        elif ty == Tm01:
+            return self.tm_01
+        elif ty == Tm_10:
+            return self.tm_10
+        elif ty == Tm02:
+            return self.tm_02
+
+class JONSWAP(SpecCoeff): pass
+class Phillips(SpecCoeff): pass
 
 class Tp(T):
-    _tp_: float
+    js = SpecCoeff(tp=None,
+                      tm_01=0.098,
+                      tm_10=0.093,
+                      tm_02=0.096,
+                      tm_03=0.093)
 
-    def __init__(self, tp):
-        self._tp_ = tp
 
-    def tp(self, spectrum: Spectrum):
-        match spectrum:
-            case Spectrum.JONSWAP:
-                return self._tp_
+class Tm01(T):
+    js = SpecCoeff(tp=1.,
+                      tm_01=None,
+                      tm_10=0.093,
+                      tm_02=0.096,
+                      tm_03=0.093)
 
-        raise ValueError()
 
-class Tm0(T):
-    _tm0_: float
+class Tm_10(T):
+    js = SpecCoeff(tp=1.,
+                      tm_01=0.098,
+                      tm_10=0.093,
+                      tm_02=0.096,
+                      tm_03=0.093)
 
-    def __init__(self, tm0):
-        self._tm0_ = tm0
 
-    def tp(self, spectrum: Spectrum):
-        match spectrum:
-            case Spectrum.JONSWAP:
-                return self._tm0_ * 1.1
+class Tm02(T):
+    js = SpecCoeff(tp=1.,
+                      tm_01=0.098,
+                      tm_10=0.093,
+                      tm_02=0.096,
+                      tm_03=0.093)
 
-        raise ValueError()
+
+class Tm03(T):
+    js = SpecCoeff(tp=1.,
+                      tm_01=0.098,
+                      tm_10=0.093,
+                      tm_02=0.096,
+                      tm_03=0.093)
+
--- tests/models/test_waveperiod.py
+++ tests/models/test_waveperiod.py
@@ -1,10 +1,23 @@
 from pytest import approx
-from opendrift.physics.waveperiod import Tm0, Tp, Spectrum
+from opendrift.physics.waveperiod import Tm01, Tp, Spectrum
 
 def test_tp():
     t = Tp(12.)
     assert t.tp(Spectrum.JONSWAP) == approx(12.)
 
 def test_tm0():
-    ta = Tm0(10.)
+    ta = Tm01(10.)
     assert ta.tp(Spectrum.JONSWAP) == approx(11.)
+
+    b = Tm01(10.).into(Tm01, Spectrum.Phillips).into(Tm02, Spectrum.JONSWAP)
+
+
+    print(b.t)
+
+
+
+
+    ###
+
+
+    c = b.into(Tp, Spectrum.Phillips)
