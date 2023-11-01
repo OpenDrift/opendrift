2e635e7a48e8bea2eb5501f8795f34bfa042e9e9
Gaute Hope <gauteh@met.no>
2023-11-01 11:11:28 +0100

    leeway ascii: try to set precission of output to see if the output is more stable

--- tests/models/test_leeway.py
+++ tests/models/test_leeway.py
@@ -48,21 +48,19 @@ def test_leeway_config_object():
     assert l.leewayprop[objType]['Description'] == 'Surf board with person'
     assert l.leewayprop[objType]['OBJKEY'] == 'PERSON-POWERED-VESSEL-2'
 
-@pytest.mark.skipif(datetime.now()<datetime(2023, 11, 1),
-                    reason='Slight differences might be due to temporary library differences')
 def test_leewayrun(tmpdir, test_data):
     """Test the expected Leeway left/right split."""
     lee = Leeway(loglevel=20)
     object_type = 50  # FISHING-VESSEL-1
     reader_landmask = reader_global_landmask.Reader()
     lee.add_reader([reader_landmask])
-    lee.seed_cone(lon=[4.5, 4.7], lat=[60.1, 60], number=100,
-                  object_type=object_type,
-                  time=[datetime(2015, 1, 1, 0), datetime(2015, 1, 1, 6)])
     lee.set_config('environment:fallback:x_wind', 0)
     lee.set_config('environment:fallback:y_wind', 10)
     lee.set_config('environment:fallback:x_sea_water_velocity', 0)
     lee.set_config('environment:fallback:y_sea_water_velocity', 0)
+    lee.seed_cone(lon=[4.5, 4.7], lat=[60.1, 60], number=100,
+                  object_type=object_type,
+                  time=[datetime(2015, 1, 1, 0), datetime(2015, 1, 1, 6)])
     # Check that 10 out of 100 elements strand towards coast
     lee.run(steps=24, time_step=3600)
     assert lee.num_elements_scheduled() == 0
