857fb75a598f186993aa93b6edb62acf1526885e
student34 <ugo.martinez@live.fr>
2024-05-27 16:44:54 +0200

    Modification so Reader.get_variables_interpolated() is always called with named arguments. Ad some more documentation in ops and readerops

--- opendrift/readers/operators/ops.py
+++ opendrift/readers/operators/ops.py
@@ -5,7 +5,9 @@ import pyproj
 import xarray as xr
 import numpy as np
 class Combine:
-
+    """Combining two readers into a third one. You can use usual operators,
+    but also more complex ones such as gaussian combining.
+    """
 
     def __add__(self, other):
         from .readerops import Combined as ReaderCombined
@@ -38,6 +40,10 @@ class Combine:
         return self + (-1 * other)
 
     def combine_gaussian(self, measurement_reader, std):
+        """Mix two readers with a gaussian, whose std is the one given as an argument.
+        The measurment reader have to be of type timeseries, with a lon and lat
+        attributes that are taken as the center of the measure.
+        """
         from .readerops import Combined as ReaderCombined
 
         def gaussian_melting(a, b, lon, lat, lon_center, lat_center, std):
@@ -46,21 +52,9 @@ class Combine:
                 lon_center = lon_center * np.ones(len(lon))
             if ( isinstance(lat_center, float) or len(lat_center) == 1 ) and len(lat) != 1 :
                 lat_center = lat_center * np.ones(len(lat))
-
-            print("lon length: {}".format(len(lon)))
-            print("lat length: {}".format(len(lat)))
-            print("lon_center length: {}".format(len(lon_center)))
-            print("lat_center length: {}".format(len(lat_center)))
-
             _, _, distance = geod.inv(lon, lat, lon_center, lat_center)
             exponential_factor = np.exp( -np.power(distance/std, 2.) / 2)
             res = a * (1 - exponential_factor) + b * exponential_factor
-            print("==================================")
-            print("Gaussian calculation")
-            print("Lon, lat, lon_center, lat_center = {}".format((lon, lat, lon_center, lat_center)))
-            print("Distance used for calculation: {}".format(distance))
-            print("a = {}, b = {}, c = {}".format(a, b, res))
-            print("=================================")
             return res
 
         return ReaderCombined(a = self, b = measurement_reader, op = gaussian_melting, op_type= "combine_gaussian", external_params = std)
