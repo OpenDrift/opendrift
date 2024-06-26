509e8114bfc5c5de51d6802d53e78a4359d22ba0
student34 <ugo.martinez@live.fr>
2024-06-26 13:33:10 +0200

    From 2d claculation of skillscore to Nd calculation

--- opendrift/models/physics_methods.py
+++ opendrift/models/physics_methods.py
@@ -88,10 +88,13 @@ def distance_between_trajectories(lon1, lat1, lon2, lat2):
     return distance
 
 def distance_along_trajectory(lon, lat):
-    '''Calculate the distances [m] between points along a trajectory'''
+    '''Calculate the distances [m] between points along a trajectory
+    assuming the last dimension is the time dimension for the
+    trajectory.
+    '''
 
     geod = pyproj.Geod(ellps='WGS84')
-    azimuth_forward, a2, distance = geod.inv(lon[:,1:], lat[:,1:], lon[:,:-1], lat[:,:-1])
+    azimuth_forward, a2, distance = geod.inv(lon[...,1:], lat[...,1:], lon[...,:-1], lat[...,:-1])
 
     return distance
 
