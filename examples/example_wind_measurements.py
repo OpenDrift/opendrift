ea1a81d75c29eeca5e38a687b5522cb5d35143bd
student34 <ugo.martinez@live.fr>
2024-06-27 14:11:47 +0200

    New features for plot function and improove of the wind measurement example.

--- examples/example_wind_measurements.py
+++ examples/example_wind_measurements.py
@@ -16,8 +16,17 @@ import pyproj
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+# Preparation of figsize, dpi, fontsize and font style
+# Could also be done using mpl style sheet
+# https://matplotlib.org/stable/users/explain/customizing.html
 mpl.rcParams['figure.figsize'] = (5, 5)
 mpl.rcParams['savefig.dpi'] = 300
+mpl.rcParams['font.family'] = "sans-serif"
+mpl.rcParams['font.sans-serif'] = "DejaVu Sans"
+mpl.rcParams['font.size'] = 22
+mpl.rcParams['mathtext.fontset'] = "dejavusans"
+
+
 
 #%%
 # This example shows how to use wind or currents measurments 
@@ -26,17 +35,19 @@ mpl.rcParams['savefig.dpi'] = 300
 
 
 #%% 
-# We import two times the same model, because one
+# Fisrt, we import two times the same model, because one
 # will be used for the usual simulation, and one will
 # be used for the measurement combined simulation.
 
 reader_norkyst = reader_netCDF_CF_generic.Reader('https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
 reader_norkyst2 = reader_netCDF_CF_generic.Reader('https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
+
+#Verification
 print(reader_norkyst)
 
 
 #%% 
-# We import wind measurement data, 
+# We then import wind measurement data,
 # that will be stored in a timeserie reader.
 # Don't forget to give the lon and lat of the 
 # measurement station.
@@ -46,19 +57,17 @@ wind_speed_array = np.array([1.9, 3.0, 3.5, 3.4, 3.9, 2.8, 2.5])
 wind_speed_angle = np.array([22, 25, 24, 19, 17, 30, 16])
 
 
-####################################
-#File stored wind measurement data
-#Get this type of file with printobs 
-#https://github.com/bohlinger/printobs
 
-#data_wind_bridge = np.genfromtxt("Simulation_data/data_wind_21_mai_2024.txt", dtype = '<U10', skip_header=1)
-#temporal_time = np.array([data_wind_bridge[i, 0] + "T" + data_wind_bridge[i, 1] for i in range(len(data_wind_bridge))])
-#time_list = [datetime.fromisoformat(temporal_time[i]) for i in range(len(temporal_time))]
-#wind_speed_array = data_wind_bridge[:, 3]
-#wind_speed_angle = data_wind_bridge[:, 5]
-#wind_speed_array = wind_speed_array.astype(float)
-#wind_speed_angle = wind_speed_angle.astype(float)
-##################################
+# Alternative if data are in a csv file
+
+# data_wind_bridge = np.genfromtxt("Simulation_data/data_wind_21_mai_2024.txt", dtype = '<U10', skip_header=1)
+# temporal_time = np.array([data_wind_bridge[i, 0] + "T" + data_wind_bridge[i, 1] for i in range(len(data_wind_bridge))])
+# time_list = [datetime.fromisoformat(temporal_time[i]) for i in range(len(temporal_time))]
+# wind_speed_array = data_wind_bridge[:, 3]
+# wind_speed_angle = data_wind_bridge[:, 5]
+# wind_speed_array = wind_speed_array.astype(float)
+# wind_speed_angle = wind_speed_angle.astype(float)
+
 
 #Calculation of wind_x and wind_y
 wind_speed_angle = np.deg2rad(wind_speed_angle)
@@ -69,6 +78,11 @@ wind_y = wind_speed_array * np.sin(wind_speed_angle)
 lon, lat = 5.17, 60.37
 parameter_value_map = {'time': time_list, 'wind_speed': wind_speed_array, 'x_wind': wind_x, 'y_wind':  wind_y, 'lon':lon, 'lat':lat}
 wind_obs_reader = reader_timeseries.Reader(parameter_value_map)
+
+#Preparing future plots
+text = [{'s': '* Station', 'x': lon, 'y': lat, 'fontsize': 20, 'color': 'k', 'backgroundcolor': 'white', 'bbox': dict(facecolor='none', edgecolor='none', alpha=0.4), 'zorder': 1000}]
+
+#Verification
 print(wind_obs_reader)
 
 #%% 
@@ -89,7 +103,7 @@ time = [time_start, time_end]
 time_step = 150
 time_step_output = 150
 
-#Seeding configuration
+# Seeding configuration
 lon = 5.187
 lat = 60.385
 radius = 0
@@ -99,17 +113,18 @@ wind_drift_factor = 0.03
 
 
 #%% 
-#Usual simulation
+# Usual simulation
 
 o = OceanDrift(loglevel=20)
 o.add_reader([reader_norkyst])
 
-#o.set_config('drift:current_uncertainty', .1)
-#o.set_config('drift:wind_uncertainty', 1)
+# o.set_config('drift:current_uncertainty', .1)
+# o.set_config('drift:wind_uncertainty', 1)
 
 o.seed_elements(lon=lon, lat=lat, radius=radius, number=number, time=time_start, wind_drift_factor=wind_drift_factor)
 o.run(end_time=time_end, time_step=time_step, time_step_output=time_step_output)
-o.plot(fast = True, file = "plot_fast", background=['x_sea_water_velocity', 'y_sea_water_velocity'], buffer = .02) 
+o.plot(fast = False, background=['x_sea_water_velocity', 'y_sea_water_velocity'], legend=['Norkyst onlyt', 'Gaussian measurment'], buffer = .023, markersize = 150, linewidth = 3, title = "", xlocs = mpl.ticker.MaxNLocator(5), ylocs = mpl.ticker.MaxNLocator(5), clabel = r"Wind speed $\mathrm{(m.s^{-1})}$", cpad = 0.08, text=text)
+
 
 #%%
 # Measurement modified model simulation
@@ -124,7 +139,7 @@ o2.add_reader([c, reader_norkyst])
 o2.seed_elements(lon=lon, lat=lat, radius=radius, number=number, time=time_start, wind_drift_factor=wind_drift_factor)
 o2.run(end_time=time_end, time_step=time_step, time_step_output=time_step_output)
 
-o.plot(fast = True, compare=o2, background=['x_sea_water_velocity', 'y_sea_water_velocity'], legend=['Norkyst onlyt', 'Gaussian measurment'], buffer = .02, markersize = 70, linewidth = 1)
+o.plot(fast = False, compare=o2, background=['x_sea_water_velocity', 'y_sea_water_velocity'], legend=['Norkyst onlyt', 'Gaussian measurment'], buffer = .023, markersize = 150, linewidth = 3, title = "", xlocs = mpl.ticker.MaxNLocator(5), ylocs = mpl.ticker.MaxNLocator(5), clabel = r"Wind speed $\mathrm{(m.s^{-1})}$", cpad = 0.08, text=text)
 
 
 #%%
@@ -146,7 +161,8 @@ o2.add_reader([c, reader_norkyst])
 o2.seed_elements(lon=lon, lat=lat, radius=radius, number=number, time=time_start, wind_drift_factor=wind_drift_factor)
 o2.run(end_time=time_end, time_step=time_step, time_step_output=time_step_output)
 
-o.plot(fast = True, compare=o2, background=['x_sea_water_velocity', 'y_sea_water_velocity'], legend=['Norkyst onlyt', 'Gaussian measurment'], buffer = .02, markersize = 70, linewidth = 1)
+o.plot(fast = False, compare=o2, background=['x_sea_water_velocity', 'y_sea_water_velocity'], legend=['Norkyst onlyt', 'Gaussian measurment'], buffer = .023, markersize = 70, linewidth = 1, title = "", xlocs = mpl.ticker.MaxNLocator(5), ylocs = mpl.ticker.MaxNLocator(5), clabel = r"Wind speed $\mathrm{(m.s^{-1})}$", cpad = 0.08, text=text)
+
 
 lon_drifters_1, lat_drifters_1 = o.get_lonlats()
 lon_drifters_2, lat_drifters_2 = o2.get_lonlats()
@@ -155,6 +171,12 @@ skillscores = phy.skillscore_liu_weissberg(lon_drifters_1, lat_drifters_1, lon_d
 
 plt.figure()
 plt.hist(skillscores, bins = 100, range = (0, 1), facecolor = 'none', edgecolor = 'C0')
+plt.xlabel("Skillscore")
+plt.ylabel("Histogram (percentages)")
+plt.show()
+
+plt.figure()
+plt.hist(skillscores, bins = 100, range = (0.9, 0.95), facecolor = 'none', edgecolor = 'C0')
 plt.xlabel("Skillscore")
 plt.ylabel("Histogram (percentages)")
 plt.show()
