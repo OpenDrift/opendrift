f472b3f73f5a1a8a36e05b57b33b79bfcd4586ae
Achref Othmani <achoth@Jakarta.ad.nersc.no>
2024-06-28 09:53:24 +0200

    #1341 updated icebergs module

--- examples/example_openberg_new.py
+++ examples/example_openberg_new.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 """
-Ice bergs (openberg)
+Icebergs (openberg)
 ====================
 """
 
-from datetime import datetime, timedelta
 from opendrift.models.openberg import OpenBerg
-
-o = OpenBerg()
+from datetime import datetime,timedelta
+ 
+o = OpenBerg(add_stokes_drift = True,
+             wave_rad = True,
+             grounding = False,
+             vertical_profile = False,
+             melting = False,
+             choose_melting  = {"wave": True, "lateral": True, "basal": True})
 
 o.add_readers_from_list([
         'https://thredds.met.no/thredds/dodsC/cmems/topaz6/dataset-topaz6-arc-15min-3km-be.ncml',
         'https://pae-paha.pacioos.hawaii.edu/thredds/dodsC/ncep_global/NCEP_Global_Atmospheric_Model_best.ncd'])
 
-o.seed_elements(lon=28, lat=77, time=datetime.now(), number=1000, radius=1000)
 
-o.run(duration=timedelta(days=2))
-
+o.seed_elements(lon= -57,lat= 69,time=datetime.now(), number=1000, radius=1000)
+o.run(duration=timedelta(days=2)) 
 o.plot(fast=True)
