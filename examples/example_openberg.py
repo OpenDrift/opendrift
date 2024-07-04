fc0f5d60e2f8521c84f04b3c00d9933ee335b64f
Achref Othmani <achoth@Jakarta.wifi.ad.nersc.no>
2024-07-04 16:09:08 +0200

    #1355 corrected version icebergs module

--- examples/example_openberg.py
+++ examples/example_openberg.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 """
-Icebergs (OpenBerg module)
-==========================
+Icebergs (openberg)
+====================
 """
 
 from opendrift.models.openberg import OpenBerg
 from datetime import datetime,timedelta
  
-o = OpenBerg(add_stokes_drift = True,
-             wave_rad = True,
-             grounding = False,
-             vertical_profile = False,
-             melting = False,
-             choose_melting  = {"wave": True, "lateral": True, "basal": True})
+o = OpenBerg()
+
+# The user can overwrite the default setup using set_config method
+o.set_config('drift:vertical_profile', False) # use surface currents for this test
+
+
 
 o.add_readers_from_list([
         'https://thredds.met.no/thredds/dodsC/cmems/topaz6/dataset-topaz6-arc-15min-3km-be.ncml',
         'https://pae-paha.pacioos.hawaii.edu/thredds/dodsC/ncep_global/NCEP_Global_Atmospheric_Model_best.ncd'])
 
 
-o.seed_elements(lon= -57,lat= 69,time=datetime.now(), number=1000, radius=1000)
-o.run(duration=timedelta(days=2)) 
+o.seed_elements(lon= -56,lat= 72,time=datetime.now(),
+                number=100, radius=500,
+                sail=10,draft=50,length=90,width=40)
+
+o.run(duration=timedelta(days=3))
 o.plot(fast=True)
+o.plot_property('draft')
+
