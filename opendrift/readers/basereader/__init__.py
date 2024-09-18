74c88abe372683a87a3846adcaca7591f13af0d5
Knut-Frode Dagestad <knutfd@met.no>
2024-09-18 10:00:32 +0200

    Added some aliases for sea_surface_height

--- opendrift/readers/basereader/__init__.py
+++ opendrift/readers/basereader/__init__.py
@@ -75,6 +75,11 @@ class BaseReader(Variables, Combine, Filter):
         'ocean_mixed_layer_thickness_defined_by_sigma_theta': 'ocean_mixed_layer_thickness',
         'sea_floor_depth_below_sea_surface' : 'sea_floor_depth_below_sea_level',
         'sea_floor_depth_below_geoid' : 'sea_floor_depth_below_sea_level',
+        'sea_surface_elevation': 'sea_surface_height',
+        'sea_surface_elevation_anomaly': 'sea_surface_height',
+        'sea_surface_height_above_mean_sea_level': 'sea_surface_height',
+        'sea_surface_height_above_sea_level': 'sea_surface_height',
+        'sea_surface_height_above_geoid': 'sea_surface_height',
         'mass_concentration_of_suspended_matter_in_sea_water' : 'spm'
         }
 
