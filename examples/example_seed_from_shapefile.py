0423cb2bcc5548abbeefa117961a306ba097b88d
Knut-Frode Dagestad <knutfd@met.no>
2025-05-08 17:49:49 +0200

    [run-ex] Seeding from shapefiles is now based on geojson, as new dependency. Removed some redundancy with new method seed_from_geopandas, which is used by both seed_from_shapefile and seed_from_wkt

--- examples/example_seed_from_shapefile.py
+++ examples/example_seed_from_shapefile.py
@@ -5,32 +5,23 @@ Seeding from shapefile
 """
 
 from datetime import datetime
+import geopandas as gpd
 from opendrift.models.oceandrift import OceanDrift
 
 
 o = OceanDrift(loglevel=20)  # Set loglevel to 0 for debug information
-o.set_config('environment:fallback:x_wind', -4)  # Constant wind drift
-o.set_config('environment:fallback:y_wind', 8)
-o.set_config('drift:wind_uncertainty', 4) # Adding some diffusion
-o.set_config('drift:vertical_mixing', False)
 
 #%%
-# Seed particles within contours from shapefile
+# Seed particles within all polygons from shapefile
 o.seed_from_shapefile(o.test_data_folder() +
                       'shapefile_spawning_areas/Torsk.shp',
-                      number=2000, layername=None,
-                      featurenum=[2, 4], time=datetime.utcnow())
+                      number=1000, time=datetime.utcnow())
 
 
 #%%
-# Running model
-o.run(steps=50, time_step=3600)
+# Alternative by using GeoPandas and selecting a subset of two polygons
+gdf = gpd.read_file(o.test_data_folder() + 'shapefile_spawning_areas/Torsk.shp')
+gdf = gdf.iloc[[1, 5]]
+o.seed_from_geopandas(gdf, number=2000, time=datetime.utcnow())
 
-#%%
-# Print and plot results
-print(o)
 o.plot(fast=True)
-o.animation(fast=True)
-
-#%%
-# .. image:: /gallery/animations/example_seed_from_shapefile_0.gif
